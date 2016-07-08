3020c18e65950a022d0f8c787ee468bae04da775
Pierre-Yves Gerardy <py@py.gy>
2016-07-09 00:29:06 +0200

    Add Mithril v1pre

---
+++ mithril-v1pre/src/main.es6.js
@@ -0,0 +1,8 @@
+'use strict';
+/** @jsx m */
+var m = require('mithril')
+//controller
+var { Controller } = require('./controller');
+
+//initialize
+m.mount(document.getElementById("main"), Controller);
