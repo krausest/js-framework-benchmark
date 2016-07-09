b11bf86491e7fa9e0c638321f03480cf1e4da045
Stefan Krause <Stefan.Krause@gmx.at>
2016-07-09 08:49:42 +0200

    Added console logging to vue

--- vue-v1.0.26/src/main.es6.js
+++ vue-v1.0.26/src/main.es6.js
@@ -5,6 +5,23 @@ var {Store} = require('./store');
 
 var store = new Store();
 
+var startTime;
+var lastMeasure;
+var startMeasure = function(name) {
+    startTime = performance.now();
+    lastMeasure = name;
+}
+var stopMeasure = function() {
+    var last = lastMeasure;
+    if (lastMeasure) {
+        window.setTimeout(function () {
+            lastMeasure = null;
+            var stop = performance.now();
+            console.log(last+" took "+(stop-startTime));
+        }, 0);
+    }
+}
+
 new Vue({
     el: '#main',
     data: {
@@ -13,44 +30,60 @@ new Vue({
     },
     methods: {
         add() {
+            startMeasure("add");
             store.add();
             this.rows = store.data;
             this.selected = store.selected;
+            stopMeasure();
         },
         remove(item) {
+            startMeasure("remove");
             store.delete(item.id);
             this.rows = store.data;
             this.selected = store.selected;
+            stopMeasure();
         },
         select(item) {
+            startMeasure("select");
             store.select(item.id);
             this.rows = store.data;
             this.selected = store.selected;
+            stopMeasure();
         },
         run() {
+            startMeasure("run");
             store.run();
             this.rows = store.data;
             this.selected = store.selected;
+            stopMeasure();
         },
         update() {
+            startMeasure("update");
             store.update();
             this.rows = store.data;
             this.selected = store.selected;
+            stopMeasure();
         },
         runLots() {
+            startMeasure("runLots");
             store.runLots();
             this.rows = store.data;
             this.selected = store.selected;
+            stopMeasure();
         },
         clear() {
+            startMeasure("clear");
             store.clear();
             this.rows = store.data;
             this.selected = store.selected;
+            stopMeasure();
         },
         swapRows() {
+            startMeasure("swapRows");
             store.swapRows();
             this.rows = store.data;
             this.selected = store.selected;
+            stopMeasure();
         }
     }
 })
