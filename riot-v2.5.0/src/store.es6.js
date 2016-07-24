f6f5b7bc99217211e9e6206ccb17fedc3a8c641b
Gianluca Guarini <gianluca.guarini@gmail.com>
2016-07-24 16:21:12 +0200

    Fix the riot bench avoiding template errors and other useless stuff

--- riot-v2.5.0/src/store.es6.js
+++ riot-v2.5.0/src/store.es6.js
@@ -1,94 +1,77 @@
-'use strict';
+'use strict'
 
 function _random(max) {
-    return Math.round(Math.random()*1000)%max;
+    return Math.round(Math.random()*1000)%max
 }
 
-var startTime;
-var lastMeasure;
-var startMeasure = function(name) {
-    startTime = performance.now();
-    lastMeasure = name;
-}
-var stopMeasure = function() {
-    var last = lastMeasure;
-    if (lastMeasure) {
-        window.setTimeout(function () {
-            lastMeasure = null;
-            var stop = performance.now();
-            var duration = 0;
-            console.log(last+" took "+(stop-startTime));
-        }, 0);
-    }
-}
-
-export class Store {
+export default class Store {
     constructor() {
-        this.data = [];
-        this.selected = undefined;
-        this.id = 1;
+        this.data = []
+        this.selected = undefined
+        this.id = 1
     }
     buildData(count = 1000) {
-        var adjectives = ["pretty", "large", "big", "small", "tall", "short", "long", "handsome", "plain", "quaint", "clean", "elegant", "easy", "angry", "crazy", "helpful", "mushy", "odd", "unsightly", "adorable", "important", "inexpensive", "cheap", "expensive", "fancy"];
-        var colours = ["red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black", "orange"];
-        var nouns = ["table", "chair", "house", "bbq", "desk", "car", "pony", "cookie", "sandwich", "burger", "pizza", "mouse", "keyboard"];
-        var data = [];
-        for (var i = 0; i < count; i++)
-            data.push({id: this.id++, label: adjectives[_random(adjectives.length)] + " " + colours[_random(colours.length)] + " " + nouns[_random(nouns.length)] });
-        return data;
+        var adjectives = ['pretty', 'large', 'big', 'small', 'tall', 'short', 'long', 'handsome', 'plain', 'quaint', 'clean', 'elegant', 'easy', 'angry', 'crazy', 'helpful', 'mushy', 'odd', 'unsightly', 'adorable', 'important', 'inexpensive', 'cheap', 'expensive', 'fancy']
+        var colours = ['red', 'yellow', 'blue', 'green', 'pink', 'brown', 'purple', 'brown', 'white', 'black', 'orange']
+        var nouns = ['table', 'chair', 'house', 'bbq', 'desk', 'car', 'pony', 'cookie', 'sandwich', 'burger', 'pizza', 'mouse', 'keyboard']
+        var data = []
+        for (var i = 0; i < count; i++) {
+            data.push({id: this.id++, label: adjectives[_random(adjectives.length)] + ' ' + colours[_random(colours.length)] + ' ' + nouns[_random(nouns.length)] })
+        }
+        return data
     }
     updateData(mod = 10) {
-        for (let i=0;i<this.data.length;i+=10) {
-            this.data[i].label += ' !!!';
+        // Just assigning setting each tenth this.data doesn't cause a redraw, the following does:
+        var newData = []
+        for (let i = 0; i < this.data.length; i ++) {
+            if (i%10===0) {
+                newData[i] = Object.assign({}, this.data[i], {label: this.data[i].label + ' !!!'})
+            } else {
+                newData[i] = this.data[i]
+            }
         }
+        this.data = newData
     }
     delete(id) {
-        startMeasure("delete");
-        const idx = this.data.findIndex(d => d.id==id);
-        this.data.splice(idx, 1);
-        stopMeasure();
-        return this;
+        const idx = this.data.findIndex(d => d.id==id)
+        this.data.splice(idx, 1)
     }
     run() {
-        startMeasure("run");
-        this.data = this.buildData();
-        this.selected = undefined;
-        stopMeasure();
+        this.data = this.buildData()
+        this.selected = undefined
     }
     add() {
-        startMeasure("add");
-        this.data = this.data.concat(this.buildData(1000));
-        stopMeasure();
+        this.data = this.data.concat(this.buildData(1000))
     }
     update() {
-        startMeasure("update");
-        this.updateData();
-        stopMeasure();
+        this.updateData()
     }
     select(id) {
-        startMeasure("select");
-        this.selected = id;
-        stopMeasure();
+        this.selected = id
     }
     runLots() {
-        startMeasure("runLots");
-        this.data = this.buildData(10000);
-        this.selected = undefined;
-        stopMeasure();
+        this.data = this.buildData(10000)
+        this.selected = undefined
     }
     clear() {
-        startMeasure("clear");
-        this.data = [];
-        this.selected = undefined;
-        stopMeasure();
+        this.data = []
+        this.selected = undefined
     }
     swapRows() {
-        startMeasure("swapRows");
         if(this.data.length > 10) {
-            var a = this.data[4];
-            this.data[4] = this.data[9];
-            this.data[9] = a;
+            let d4 = this.data[4]
+            let d9 = this.data[9]
+
+            var newData = this.data.map(function(data, i) {
+                if(i === 4) {
+                    return d9
+                }
+                else if(i === 9) {
+                    return d4
+                }
+                return data
+            })
+            this.data = newData
         }
-        stopMeasure();
     }
 }
