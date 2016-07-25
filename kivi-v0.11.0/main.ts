f73e4060f6efc97b9d55fa03a1850b83246cb0b1
Boris Kaul <localvoid@gmail.com>
2016-07-26 00:54:29 +0700

    Add kivi v0.11.0

---
+++ kivi-v0.11.0/main.ts
@@ -0,0 +1,295 @@
+import { ComponentDescriptor, VModel, createVElement, injectComponent, clock, scheduleMicrotask,
+  matchesWithAncestors } from "kivi";
+
+let startTime = 0;
+let lastMeasure: string | null;
+
+function startMeasure(name: string) {
+  startTime = performance.now();
+  lastMeasure = name;
+}
+
+function stopMeasure() {
+  const last = lastMeasure;
+  if (lastMeasure) {
+    window.setTimeout(function () {
+      const now = performance.now();
+      lastMeasure = null;
+      console.log(`${last} took ${now - startTime}`);
+    }, 0);
+  }
+}
+
+function _random(max: number): number {
+  return Math.round(Math.random() * 1000) % max;
+}
+
+const Adjectives = ["pretty", "large", "big", "small", "tall", "short", "long", "handsome", "plain", "quaint", "clean",
+                    "elegant", "easy", "angry", "crazy", "helpful", "mushy", "odd", "unsightly", "adorable",
+                    "important", "inexpensive", "cheap", "expensive", "fancy"];
+const Colors = ["red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black", "orange"];
+const Nouns = ["table", "chair", "house", "bbq", "desk", "car", "pony", "cookie", "sandwich", "burger", "pizza",
+               "mouse", "keyboard"];
+
+interface RowData {
+  id: number;
+  label: string;
+  mtime: number;
+}
+
+let nextId = 0;
+
+function buildRowData(count = 1000): RowData[] {
+  const data = new Array(count);
+  for (let i = 0; i < count; i++) {
+    data[i] = ({
+      id: nextId++,
+      label: `${Adjectives[_random(Adjectives.length)]} ` +
+             `${Colors[_random(Colors.length)]} ` +
+             `${Nouns[_random(Nouns.length)]}`,
+      mtime: -1,
+    });
+  }
+
+  return data;
+}
+
+class Store {
+  data: RowData[];
+  selected: RowData | null;
+  onChange: (() => void) | null;
+
+  constructor() {
+    this.data = [];
+    this.selected = null;
+    this.onChange = null;
+  }
+
+  invalidate() {
+    this.onChange!();
+  }
+
+  delete(row: RowData) {
+    this.data.splice(this.data.indexOf(row), 1);
+    this.invalidate();
+  }
+
+  run() {
+    this.data = buildRowData();
+    this.selected = null;
+    this.invalidate();
+  }
+
+  add() {
+    this.data = this.data.concat(buildRowData(1000));
+    this.invalidate();
+  }
+
+  update() {
+    const data = this.data;
+    for (let i = 0; i < data.length; i += 10) {
+      const r = data[i];
+      r.label += " !!!";
+      r.mtime = clock();
+    }
+
+    this.invalidate();
+  }
+
+  select(row: RowData) {
+    this.selected = row;
+    this.invalidate();
+  }
+
+  runLots() {
+    this.data = buildRowData(10000);
+    this.selected = null;
+    this.invalidate();
+  }
+
+  clear() {
+    this.data = [];
+    this.selected = null;
+    this.invalidate();
+  }
+
+  swapRows() {
+    if (this.data.length > 10) {
+      const a = this.data[4];
+      this.data[4] = this.data[9];
+      this.data[9] = a;
+    }
+    this.invalidate();
+  }
+}
+
+const store = new Store();
+
+interface RowProps {
+  data: RowData;
+  selected: boolean;
+}
+
+const RowTitle = new VModel<string>("td")
+  .enableCloning()
+  .className("col-md-1")
+  .updateHandler((e, oldProps, newProps) => {
+    if (oldProps === undefined || oldProps !== newProps) {
+      e.textContent = newProps;
+    }
+  });
+
+const HiddenAttrs = {"aria-hidden": "true"};
+
+const RemoveIcon = new VModel<void>("span")
+  .enableCloning()
+  .className("glyphicon glyphicon-remove")
+  .attrs(HiddenAttrs);
+
+const Row = new ComponentDescriptor<RowProps, void>()
+  .enableBackRef()
+  .tagName("tr")
+  .newPropsReceived((c, oldProps, newProps) => {
+    if (oldProps.selected !== newProps.selected || c.mtime < newProps.data.mtime) {
+      c.markDirty();
+    }
+  })
+  .update((c, props) => {
+    const data = props.data;
+
+    c.vSync(c.createVRoot().className(props.selected ? "danger" : "x").children([
+      RowTitle.createVNode().data(data.id),
+      createVElement("td").className("col-md-4").children([
+        createVElement("a").children(data.label),
+      ]),
+      createVElement("td").className("col-md-1").children([
+        createVElement("a").children([
+          RemoveIcon.createVNode(),
+        ]),
+      ]),
+      createVElement("td").className("col-md-6"),
+    ]));
+  });
+
+const onRowClick = Row.createDelegatedEventHandler(".col-md-4 > a", "tr", (e, c, props) => {
+  startMeasure("select");
+  store.select(props.data);
+});
+
+const onRowDelete = Row.createDelegatedEventHandler(".col-md-1 > a", "tr", (e, c, props) => {
+  startMeasure("delete");
+  store.delete(props.data);
+});
+
+function rowClick(e: MouseEvent) {
+  onRowClick(e);
+  onRowDelete(e);
+}
+
+const ButtonProps = {type: "button"};
+
+const Menu = new ComponentDescriptor()
+  .enableBackRef()
+  .attached((c) => {
+    (c.element as HTMLElement).onclick = (e) => {
+      e.stopPropagation();
+      if (matchesWithAncestors(e.target as Element, ".c1k", c.element)) {
+        startMeasure("run");
+        store.run();
+      } else if (matchesWithAncestors(e.target as Element, ".c10k", c.element)) {
+        startMeasure("runLots");
+        store.runLots();
+      } else if (matchesWithAncestors(e.target as Element, ".a1k", c.element)) {
+        startMeasure("add");
+        store.add();
+      } else if (matchesWithAncestors(e.target as Element, ".u10th", c.element)) {
+        startMeasure("update");
+        store.update();
+      } else if (matchesWithAncestors(e.target as Element, ".cl", c.element)) {
+        startMeasure("clear");
+        store.clear();
+      } else if (matchesWithAncestors(e.target as Element, ".sw", c.element)) {
+        startMeasure("swapRows");
+        store.swapRows();
+      }
+    };
+  })
+  .update((c) => {
+    c.vSync(c.createVRoot().className("jumbotron").children([
+      createVElement("div").className("row").children([
+        createVElement("div").className("col-md-6").children([
+          createVElement("h1").children("kivi v0.11.0"),
+        ]),
+        createVElement("div").className("col-md-6").children([
+          createVElement("div").className("row").children([
+            createVElement("div").className("col-sm-6 smallpad").children([
+              createVElement("button").className("c1k btn btn-primary btn-block").props(ButtonProps)
+                .children("Create 1,000 rows"),
+            ]),
+            createVElement("div").className("col-sm-6 smallpad").children([
+              createVElement("button").className("c10k btn btn-primary btn-block").props(ButtonProps)
+                .children("Create 10,000 rows"),
+            ]),
+            createVElement("div").className("col-sm-6 smallpad").children([
+              createVElement("button").className("a1k btn btn-primary btn-block").props(ButtonProps)
+                .children("Append 1,000 rows"),
+            ]),
+            createVElement("div").className("col-sm-6 smallpad").children([
+              createVElement("button").className("u10th btn btn-primary btn-block").props(ButtonProps)
+                .children("Update every 10th row"),
+            ]),
+            createVElement("div").className("col-sm-6 smallpad").children([
+              createVElement("button").className("cl btn btn-primary btn-block").props(ButtonProps)
+                .children("Clear"),
+            ]),
+            createVElement("div").className("col-sm-6 smallpad").children([
+              createVElement("button").className("sw btn btn-primary btn-block").props(ButtonProps)
+                .children("Swap Rows"),
+            ]),
+          ]),
+        ]),
+      ]),
+    ]));
+  });
+
+const Table = new ComponentDescriptor<Store, void>()
+  .tagName("table")
+  .attached((c) => {
+    (c.element as HTMLElement).onclick = rowClick;
+  })
+  .update((c, props) => {
+    const data = props.data;
+    const children = new Array(data.length);
+    for (let i = 0; i < data.length; i++) {
+      const r = data[i];
+      children[i] = Row.createVNode({
+        selected: props.selected === r,
+        data: r,
+      }).key(r.id);
+    }
+
+    c.vSync(c.createVRoot().className("table table-hover table-striped test-data").children([
+      createVElement("tbody").trackByKeyChildren(children),
+    ]));
+  });
+
+const Main = new ComponentDescriptor()
+  .update((c, props, state) => {
+    c.vSync(c.createVRoot().className("container").children([
+      Menu.createImmutableVNode(),
+      Table.createVNode(store),
+      createVElement("span").className("preloadicon glyphicon glyphicon-remove").attrs(HiddenAttrs),
+    ]));
+
+    stopMeasure();
+    scheduleMicrotask(() => {
+      // Increment scheduler clock.
+      // Because of the benchmark we need to force sync updates, and because of this internal clock isn't increasing.
+    });
+  });
+
+const c = injectComponent(Main, document.getElementById("main")!, undefined, true);
+store.onChange = function() {
+  c.markDirty();
+  c.update();
+};
