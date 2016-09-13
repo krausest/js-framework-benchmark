1815f5e4149a4a84a55cdd6e5cadf64d540c691f
Matthew Beale <matt.beale@madhatted.com>
2016-09-13 07:36:39 -0700

    Refactor the Ember 2.6.1 app

--- ember-v2.6.1/app/helpers/is-selected.js
+++
@@ -1,5 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Handlebars.makeBoundHelper(function() {
-  return "u";
-});
--- ember-v2.6.1/app/routes/index.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  rows: Ember.inject.service('rows'),
-  model() {
-    return this.get('rows');
-  },
-});
