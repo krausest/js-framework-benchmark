e6d41ce9100cce814428714ac2a6904e07b6259c
Stefan Krause <Stefan.Krause@gmx.at>
2016-11-17 20:46:57 +0100

    revert angular changes

--- angular-v2.0.0/lib/main.aot.js
+++
@@ -1,7 +0,0 @@
-import 'zone.js/dist/zone';
-import { platformBrowser } from '@angular/platform-browser';
-import { enableProdMode } from '@angular/core';
-import { AppModuleNgFactory } from './app.ngfactory';
-enableProdMode();
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
-//# sourceMappingURL=main.aot.js.map
--- angular-v2.0.0/lib/main.jit.js
+++
@@ -1,8 +0,0 @@
-import 'core-js/es7/reflect';
-import 'zone.js/dist/zone';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { AppModule } from './app';
-enableProdMode();
-platformBrowserDynamic().bootstrapModule(AppModule);
-//# sourceMappingURL=main.jit.js.map
