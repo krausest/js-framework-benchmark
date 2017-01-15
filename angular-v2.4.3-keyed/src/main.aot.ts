9a4248b08eab69ae7756e71a72778e8e07fa28e6
Stefan Krause <Stefan.Krause@gmx.at>
2017-01-15 10:38:19 +0100

    update angular to v2.4.3

--- angular-v2.4.3-keyed/src/main.aot.ts
+++ angular-v2.4.3-keyed/src/main.aot.ts
@@ -1,4 +1,5 @@
 import 'zone.js/dist/zone';
+import 'core-js/es7/reflect';
 import { platformBrowser } from '@angular/platform-browser';
 import { enableProdMode } from '@angular/core';
 import { AppModuleNgFactory } from './app.ngfactory';
