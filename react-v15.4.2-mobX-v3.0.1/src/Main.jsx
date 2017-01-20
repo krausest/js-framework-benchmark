9c09e3abcd648a2f2d856aa66455a9d1c3d8b8bd
Stefan Krause <Stefan.Krause@gmx.at>
2017-01-20 22:29:57 +0100

    updated react + mobx to 15.4.2 + 3.0.1

--- react-v15.4.2-mobX-v3.0.1/src/Main.jsx
+++ react-v15.4.2-mobX-v3.0.1/src/Main.jsx
@@ -91,7 +91,7 @@ export class Main extends React.Component{
             <div className="jumbotron">
                 <div className="row">
                     <div className="col-md-6">
-                        <h1>React v15.4.0 + Mobx 2.6.3</h1>
+                        <h1>React v15.4.2 + Mobx 3.0.1</h1>
                     </div>
                     <div className="col-md-6">
                         <div className="row">
