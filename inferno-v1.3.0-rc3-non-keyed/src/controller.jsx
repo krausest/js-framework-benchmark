7e671512aab49a97d85fad9570267a484a311767
Martin Oppitz <github@martinoppitz.com>
2017-03-06 02:54:24 +0100

    rename: inferno 1.3.0-rc.10 to 1.3.0-rc

--- inferno-v1.3.0-rc3-non-keyed/src/controller.jsx
+++ inferno-v1.3.0-rc3-non-keyed/src/controller.jsx
@@ -91,7 +91,7 @@ function createRows(store, deleteFunc, selectFunc) {
 
 const title = (
     <div className="col-md-6" noNormalize>
-        <h1 noNormalize>Inferno v1.3.0-rc3 - non-keyed</h1>
+        <h1 noNormalize>Inferno v1.3.0-rc - non-keyed</h1>
     </div>
 );
 const span2 = <span className="preloadicon glyphicon glyphicon-remove" aria-hidden="true" noNormalize></span>;
--- webdriver-ts/src/common.ts
+++ webdriver-ts/src/common.ts
@@ -1,7 +1,7 @@
 export interface JSONResult {
     framework: string, benchmark: string, type: string, min: number,
-        max: number, mean: number, geometricMean: number,
-        standardDeviation: number
+    max: number, mean: number, geometricMean: number,
+    standardDeviation: number
 }
 
 export let config = {
@@ -23,59 +23,60 @@ export interface FrameworkData {
 
 interface Options {
     uri: string;
-    useShadowRoot? : boolean;
+    useShadowRoot?: boolean;
 }
 
-function f(name: string, nonKeyed: boolean, options: Options = {uri: null, useShadowRoot: false}): FrameworkData {
-    let ret = {name, nonKeyed, uri: options.uri ? options.uri : name, useShadowRoot: options.useShadowRoot};
+function f(name: string, nonKeyed: boolean, options: Options = { uri: null, useShadowRoot: false }): FrameworkData {
+    let ret = { name, nonKeyed, uri: options.uri ? options.uri : name, useShadowRoot: options.useShadowRoot };
     return ret;
 }
 
 export let frameworks = [
-    f("angular-v1.6.2-keyed", false),
-    f("angular-v2.4.9-keyed", false),
-    f("angular-v2.4.9-non-keyed", true),
-    f("angular-v4.0.0-rc-keyed", false),
-    f("aurelia-v1.0.8", true, {uri: "aurelia-v1.0.8/dist"}),
-    f("binding.scala-v10.0.1", false, {uri: "binding.scala-v10.0.1/target/web/stage"}),
-    f("bobril-v5.0.4", false),
-    f("choo-v4.1.0", true),
-    f("cyclejs-dom-v14.1.0", true),
-    f("dio-v3.0.5", true),
-    f("domvm-v2.0.1-non-keyed", true),
-    f("domvm-v2.0.1-keyed", false),
-    f("ember-v2.6.1", false, {uri: "ember-v2.6.1/dist"}),
-    f("ember-v2.10.0-beta.3", false, {uri: "ember-v2.10.0-beta.3/dist"}),
-    f("elm-v0.18.0", false),
-    f("hyperapp-v0.6.0", true),
-    f("inferno-v1.3.0-rc3-non-keyed", true),
+    // f("angular-v1.6.2-keyed", false),
+    // f("angular-v2.4.9-keyed", false),
+    // f("angular-v2.4.9-non-keyed", true),
+    // f("angular-v4.0.0-rc-keyed", false),
+    // f("aurelia-v1.1.0-keyed", true, { uri: "aurelia-v1.1.0-keyed/dist" }),
+    // f("binding.scala-v10.0.1", false, {uri: "binding.scala-v10.0.1/target/web/stage"}),
+    // f("bobril-v5.0.4", false),
+    // f("choo-v4.1.0", true),
+    // f("cyclejs-dom-v14.1.0", true),
+    // f("dio-v3.0.5", true),
+    // f("domvm-v2.0.1-non-keyed", true),
+    // f("domvm-v2.0.1-keyed", false),
+    // f("ember-v2.6.1", false, {uri: "ember-v2.6.1/dist"}),
+    // f("ember-v2.10.0-beta.3", false, {uri: "ember-v2.10.0-beta.3/dist"}),
+    // f("elm-v0.18.0", false),
+    // f("hyperapp-v0.6.0", true),
     f("inferno-v1.3.0-rc3-keyed", false),
-    f("kivi-v1.0.0-rc2", false),
-    f("knockout-v3.4.1", false),
-    f("marionette-v3.1.0", false),
-    f("mithril-v0.2.5", false),
-    f("mithril-v1.0.0-alpha", false),
-    f("nx-v1.0.0-beta.1.1.0-keyed", false),
-    f("nx-v1.0.0-beta.1.1.0-non-keyed", true),
-    f("plastiq-v1.33.0", false),
-    f("polymer-v1.7.0", true, {uri: "polymer-v1.7.0", useShadowRoot: true}),
-    f("preact-v7.1.0", false),
-    f("svelte-v1.0.1", true),
-    f("ractive-v0.8.9-keyed", false),
-    f("ractive-v0.8.9-non-keyed", true),
-    f("ractive-edge", true),
-    f("react-lite-v0.15.30", false),
-    f("react-v15.4.2-keyed", false),
-    f("react-v15.4.2-non-keyed", true),
-    f("react-v15.4.2-mobX-v3.0.1", false),
-    f("react-v15.4.2-redux-v3.6.0", false),
-    f("riot-v3.0.7", true),
-    f("simulacra-v1.5.5", true),
-    f("stem-v0.2.60", true),
-    f("tsers-v1.0.0", true),
-    f("vanillajs-non-keyed", true),
+    // f("inferno-v1.3.0-rc3-non-keyed", true),
+    // f("kivi-v1.0.0-rc2", false),
+    // f("knockout-v3.4.1", false),
+    // f("marionette-v3.1.0", false),
+    // f("mithril-v0.2.5", false),
+    // f("mithril-v1.0.0-alpha", false),
+    // f("nx-v1.0.0-beta.1.1.0-keyed", false),
+    // f("nx-v1.0.0-beta.1.1.0-non-keyed", true),
+    // f("plastiq-v1.33.0", false),
+    // f("polymer-v1.7.0", true, {uri: "polymer-v1.7.0", useShadowRoot: true}),
+    // f("preact-v7.1.0", false),
+    // f("svelte-v1.0.1", true),
+    // f("ractive-v0.8.9-keyed", false),
+    // f("ractive-v0.8.9-non-keyed", true),
+    // f("ractive-edge", true),
+    // f("react-lite-v0.15.30", false),
+    // f("react-v15.4.2-keyed", false),
+    // f("react-v15.4.2-non-keyed", true),
+    // f("react-v15.4.2-mobX-v3.0.1", false),
+    // f("react-v15.4.2-redux-v3.6.0", false),
+    // f("riot-v3.0.7", true),
+    // f("simulacra-v1.5.5", true),
+    // f("stem-v0.2.60", true),
+    // f("tsers-v1.0.0", true),
     f("vanillajs-keyed", false),
-    f("vidom-v0.7.1", false),
-    f("vue-v2.1.10-keyed", false),
-    f("vue-v2.1.10-non-keyed", true),
+    // f("vanillajs-non-keyed", true),
+    // f("vidom-v0.7.1", false),
+    // f("vue-v2.2.1-func-keyed", false),
+    // f("vue-v2.2.1-keyed", false),
+    // f("vue-v2.2.1-non-keyed", true),
 ]
