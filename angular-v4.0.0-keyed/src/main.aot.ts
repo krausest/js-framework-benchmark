7bfa69231d738ada78c33010708da04dc9b362af
Trotyl <trotyl@qq.com>
2017-05-14 21:13:55 +0800

    refactor(angular): upgrade closure compiler to advanced mode in angular-keyed

--- angular-v4.0.0-keyed/src/main.aot.ts
+++ angular-v4.0.0-keyed/src/main.aot.ts
@@ -1,5 +1,3 @@
-import 'zone.js/dist/zone';
-import 'core-js/es7/reflect';
 import { platformBrowser } from '@angular/platform-browser';
 import { enableProdMode } from '@angular/core';
 import { AppModuleNgFactory } from './app.ngfactory';
