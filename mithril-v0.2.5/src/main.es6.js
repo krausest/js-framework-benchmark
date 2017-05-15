d2f94a2eef7ad8a2375cc9472e84ac9c46d929e9
Stefan Krause <Stefan.Krause@gmx.at>
2017-05-15 22:24:48 +0200

    removed old mithril

--- mithril-v0.2.5/src/main.es6.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-/** @jsx m */
-var m = require('mithril')
-//controller
-var { Controller } = require('./controller');
-
-//initialize
-m.mount(document.getElementById("main"), Controller);
