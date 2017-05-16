c0edac56201437c72e8a2a2f623c9fdf15298cde
Stefan Krause <Stefan.Krause@gmx.at>
2017-05-16 23:11:59 +0200

    updated vue

--- webdriver-ts/src/common.ts
+++ webdriver-ts/src/common.ts
@@ -84,7 +84,7 @@ export let frameworks = [
     f("tsers-v1.0.0", true),
     f("vanillajs-non-keyed", true),
     f("vanillajs-keyed", false),
-    f("vidom-v0.7.1", false),   // remove
-    f("vue-v2.2.6-keyed", false),
-    f("vue-v2.2.6-non-keyed", true),
+    f("vidom-v0.9.8", false),   
+    f("vue-v2.3.3-keyed", false),
+    f("vue-v2.3.3-non-keyed", true),
 ]
