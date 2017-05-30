2433246e8335639dd7396677b84f10bfd96986c2
Stefan Krause <Stefan.Krause@gmx.at>
2017-05-30 20:14:37 +0200

    allow restarting build

--- build.js
+++ build.js
@@ -4,12 +4,23 @@ var fs = require('fs');
 
 var excludedDirectories = ['css', 'dist','node_modules','webdriver-java'];
 
+// set the following variable to resume building with a framework and skip all
+// other frameworks that would be buils before
+var restartWithFramework = '';
+var build = !restartWithFramework ? true : false;
+
 _.each(fs.readdirSync('.'), function(name) {
 	if(fs.statSync(name).isDirectory() && name[0] !== '.' && excludedDirectories.indexOf(name)==-1) {
-		console.log("Executing npm run build-prod in "+name);
-		exec('npm run build-prod', {
-			cwd: name,
-			stdio: 'inherit'
-		});
+		console.log("build", build, "restartWithFramework", restartWithFramework, name.startsWith(restartWithFramework));
+		if (!build && name.startsWith(restartWithFramework)) build = true;
+		if (build) {
+			console.log("Executing npm run build-prod in "+name);
+			exec('npm run build-prod', {
+				cwd: name,
+				stdio: 'inherit'
+			});
+		} else {
+			console.log("Skipping npm run build-prod in "+name);
+		}
 	}
 });
