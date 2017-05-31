aea12481733103e3cf7fa6002a2a3798e64ecbd3
Stefan Krause <Stefan.Krause@gmx.at>
2017-05-31 07:30:01 +0200

    retire angular 2

--- angular-v2.4.9-non-keyed/src/main.aot.ts
+++
@@ -1,7 +0,0 @@
-import 'zone.js/dist/zone';
-import 'core-js/es7/reflect';
-import { platformBrowser } from '@angular/platform-browser';
-import { enableProdMode } from '@angular/core';
-import { AppModuleNgFactory } from './app.ngfactory';
-enableProdMode();
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
