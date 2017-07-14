e2fc67085527f7310031b3653166cda1ad86917d
Stefan Krause <Stefan.Krause@gmx.at>
2017-07-14 18:08:21 +0200

    improve build

--- cleanup.js
+++ cleanup.js
@@ -20,24 +20,21 @@ function rmIfExists(base, name) {
 	}
 }
 
-
-commandExists('yarn', function(err, commandExists) {
-
-	_.each(fs.readdirSync('.'), function(name) {
-		if(fs.statSync(name).isDirectory() && name[0] !== '.' && excludedDirectories.indexOf(name)==-1) {
-			if(fs.existsSync(name+"/node_modules")) {
-				let cleanCommand = 'rm -r node_modules';
-				console.log("Executing "+cleanCommand+" in "+name);
-				exec(cleanCommand, {
-					cwd: name,
-					stdio: 'inherit'
-				});
-			}
-			rmIfExists(name, "package-lock.json");
-			rmIfExists(name, "yarn.lock");
-			rmIfExists(name, "dist");
-			rmIfExists(name, "elm-stuff");
-			rmIfExists(name, "bower_components");
+_.each(fs.readdirSync('.'), function(name) {
+	if(fs.statSync(name).isDirectory() && name[0] !== '.' && excludedDirectories.indexOf(name)==-1) {
+		if(fs.existsSync(name+"/node_modules")) {
+			let cleanCommand = 'rm -r node_modules';
+			console.log("Executing "+cleanCommand+" in "+name);
+			exec(cleanCommand, {
+				cwd: name,
+				stdio: 'inherit'
+			});
 		}
-	});
+		rmIfExists(name, "package-lock.json");
+		rmIfExists(name, "yarn.lock");
+		rmIfExists(name, "dist");
+		rmIfExists(name, "elm-stuff");
+		rmIfExists(name, "bower_components");
+		rmIfExists(name, "node_modules");
+	}
 });
