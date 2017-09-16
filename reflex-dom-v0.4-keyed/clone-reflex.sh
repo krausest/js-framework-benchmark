220048da4d127d1084dd9f2013d9fcfafde585f8
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2017-09-17 00:11:23 +0100

    Update remote name/url

--- reflex-dom-v0.4-keyed/clone-reflex.sh
+++ reflex-dom-v0.4-keyed/clone-reflex.sh
@@ -2,7 +2,7 @@ R=reflex-platform
 if [ ! -d "$R" ]; then
     git clone https://github.com/reflex-frp/reflex-platform.git
     cd $R
-    git remote add alexteves https://github.com/alexteves/reflex-platform.git
+    git remote add alexfmpe https://github.com/alexfmpe/reflex-platform.git
     git fetch --all
     git checkout 13d8daf368cb370d07743123d1fe66e19961c2ed
     ./hack-on reflex-dom
