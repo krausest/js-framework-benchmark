7b3f2d62755657ec2d61093169369c91cf0285de
Leon Sorokin <leeoniya@gmail.com>
2017-09-24 14:30:53 -0500

    update domvm v3.2.0

--- domvm-v3.2.0-non-keyed/build.js
+++ domvm-v3.2.0-non-keyed/build.js
@@ -4,13 +4,13 @@ const uglify = require('rollup-plugin-uglify');
 const commonjs = require('rollup-plugin-commonjs');
 
 rollup({
-	entry: 'src/main.es6.js',
+	input: 'src/main.es6.js',
 	plugins: [
 		commonjs(),
 		buble(),
 		uglify(),
 	]
 }).then(bundle => bundle.write({
-	dest: 'dist/bundle.js',
+	file: 'dist/bundle.js',
 	format: 'iife'
 })).catch(err => console.log(err.stack));
