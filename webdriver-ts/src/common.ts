4d61ea1a2ab61eecc5e50ca6a471e4f69d84a67e
Stefan Krause <Stefan.Krause@gmx.at>
2017-09-26 22:39:44 +0200

    updated to react 16.0.0

--- webdriver-ts/src/common.ts
+++ webdriver-ts/src/common.ts
@@ -85,7 +85,7 @@ export let frameworks = [
     f("react-v15.5.4-easy-state-v3.0.1-keyed", true),
     f("react-v15.5.4-mobX-v3.1.9-keyed", true),
     f("react-v15.5.4-redux-v3.6.0-keyed", true),    
-    f("react-v16.rc3", true),   
+    f("react-v16.0.0", true),   
     f("redom-v3.0.2-keyed", true),
     f("redom-v3.0.2-non-keyed", false),
     f("reflex-dom-v0.4-keyed", true, {uri: "reflex-dom-v0.4-keyed/dist"}),
