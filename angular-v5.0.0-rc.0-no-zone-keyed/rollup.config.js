e0386a6bab512c841a6344fef4e9f1945c5e1e92
Trotyl <trotyl@qq.com>
2017-09-29 16:09:08 +0800

    upgrade angular no zone.js to v5

--- angular-v5.0.0-rc.0-no-zone-keyed/rollup.config.js
+++ angular-v5.0.0-rc.0-no-zone-keyed/rollup.config.js
@@ -1,13 +1,11 @@
 import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs';
-import uglify from 'rollup-plugin-uglify'
 
 export default {
-  entry: 'tmp/main.aot.js',
+  entry: 'tmp/src/main.js',
   dest: 'dist/bundle.js',
-  sourceMap: false,
   format: 'iife',
-  moduleName: 'ng4App',
+  moduleName: 'ngApp',
   plugins: [
     nodeResolve({
       jsnext: true,
