fe3061100a962e37b28f0e48b79751085a99cea5
Simon Green <simon@captaincodeman.com>
2017-10-28 15:09:53 -0600

    Lit-HTML implementation
    https://github.com/PolymerLabs/lit-html

---
+++ lit-html-v0.7.1-non-keyed/rollup.config.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import * as path from 'path';
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/index.min.js',
+    format: 'iife',
+    name: 'app',
+    sourcemap: true,
+  },
+  plugins: [
+    resolve({
+      module: true,
+      jsnext: true,
+      browser: true,
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      presets: [
+        [ "es2016" ]
+      ],
+      plugins: [
+        "external-helpers"
+      ],
+      runtimeHelpers: true,
+      babelrc: false,
+    })
+  ],
+}
