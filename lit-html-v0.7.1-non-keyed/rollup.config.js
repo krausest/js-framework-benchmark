b5d845ddef8a2532a7ad2108fedbea8a1692655b
Simon Green <simon@captaincodeman.com>
2017-10-28 22:50:13 -0600

    Minify bundle

--- lit-html-v0.7.1-non-keyed/rollup.config.js
+++ lit-html-v0.7.1-non-keyed/rollup.config.js
@@ -3,7 +3,8 @@
 import * as path from 'path';
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
-
+import uglify from 'rollup-plugin-uglify';
+import { minify } from 'uglify-es';
 
 export default {
   input: 'src/index.js',
@@ -29,6 +30,7 @@ export default {
       ],
       runtimeHelpers: true,
       babelrc: false,
-    })
+    }),
+    uglify({}, minify),
   ],
 }
