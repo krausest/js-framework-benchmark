b4d3ebe9644b9c381fc8b6456fb724d6fa328965
Stefan Krause <Stefan.Krause@gmx.at>
2017-11-02 21:20:55 +0100

    added -keyed suffix react v16

--- webdriver-ts/src/common.ts
+++ webdriver-ts/src/common.ts
@@ -85,7 +85,7 @@ export let frameworks = [
     f("react-v16.0.0-non-keyed", false),
     f("react-v15.5.4-easy-state-v3.0.1-keyed", true),
     f("react-v15.5.4-mobX-v3.1.9-keyed", true),
-    f("react-v16.0.0", true),
+    f("react-v16.0.0-keyed", true),
     f("react-v16.0.0-redux-v3.7.2-keyed", true),
     f("redom-v3.5.0-keyed", true),
     f("redom-v3.5.0-non-keyed", false),
