66f8cd74c9a512ef3adf3fc68a14944c13ce2df7
Anthony Gubler <agubler@sitepen.com>
2017-11-08 21:25:25 +0000

    add keyed impl for dojo 2

---
+++ dojo2-v0.2.0-keyed/src/Buttons.ts
@@ -0,0 +1,32 @@
+import { WidgetBase } from '@dojo/widget-core/WidgetBase';
+import { v, w } from '@dojo/widget-core/d';
+import { WidgetProperties, DNode } from '@dojo/widget-core/interfaces';
+import { Button } from './Button';
+
+export interface ButtonConfig {
+	id: string;
+	label: string;
+	onClick: () => void;
+}
+
+export interface ButtonsProperties {
+	buttonConfigs: ButtonConfig[];
+}
+
+export class Buttons extends WidgetBase<ButtonsProperties> {
+
+	protected render(): DNode {
+		const { buttonConfigs } = this.properties;
+
+		return v('div', { classes: [ 'jumbotron' ] }, [
+			v('div', { classes: [ 'row' ] }, [
+				v('div', { classes: [ 'col-md-6' ] }, [
+					v('h1', ['Dojo2 v0.2.0'])
+				]),
+				v('div', { classes: [ 'col-md-6' ] }, buttonConfigs.map(({ id, label, onClick }) => {
+					return w(Button, { key: id, id, label, onClick });
+				}))
+			])
+		]);
+	}
+}
