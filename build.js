358dffcefb5b917ff5834f11a5122e2c5c1940d8
Stefan Krause <Stefan.Krause@gmx.at>
2017-11-12 21:36:10 +0100

    simplify build

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 var _ = require('lodash');
 var exec = require('child_process').execSync;
 var fs = require('fs');
+var path = require('path');
 
 var excludedDirectories = ['css', 'images', 'dist','node_modules','webdriver-java'];
 
@@ -10,17 +11,21 @@ var restartWithFramework = '';
 var build = !restartWithFramework ? true : false;
 
 _.each(fs.readdirSync('.'), function(name) {
-	if(fs.statSync(name).isDirectory() && name[0] !== '.' && excludedDirectories.indexOf(name)==-1) {
-		console.log("build", build, "restartWithFramework", restartWithFramework, name.startsWith(restartWithFramework));
+	if(!name.startsWith(".") && fs.statSync(name).isDirectory() && excludedDirectories.indexOf(name)==-1 && fs.existsSync(path.join(name, "package.json"))) {
 		if (!build && name.startsWith(restartWithFramework)) build = true;
 		if (build) {
-			console.log("Executing npm run build-prod in "+name);
+			console.log("*** Executing npm install in "+name);
+            exec('npm install', {
+				cwd: name,
+				stdio: 'inherit'
+			});
+			console.log("*** Executing npm run build-prod in "+name);
 			exec('npm run build-prod', {
 				cwd: name,
 				stdio: 'inherit'
 			});
 		} else {
-			console.log("Skipping npm run build-prod in "+name);
+			console.log("*** Skipping "+name);
 		}
 	}
 });
