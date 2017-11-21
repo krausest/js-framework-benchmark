0ad4b1762cac2729f7f1032344d67b03a0fde106
Stefan Krause <Stefan.Krause@gmx.at>
2017-11-21 21:39:46 +0100

    remove old react

--- react-v15.5.4-non-keyed/src/main.es6.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-let React = require('react');
-let ReactDOM = require('react-dom');
-let {Main} = require('./Main');
-
-ReactDOM.render(React.createElement(Main, {}), document.getElementById('main'));
