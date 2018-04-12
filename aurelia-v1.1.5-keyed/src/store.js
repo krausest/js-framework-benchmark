b625f76a11ebb6dc2135c9be5711c38c15b6ad71
Stefan Krause <Stefan.Krause@gmx.at>
2018-04-12 21:18:43 +0200

    results for aurelia-keyed

--- aurelia-v1.1.5-keyed/src/store.js
+++ aurelia-v1.1.5-keyed/src/store.js
@@ -28,10 +28,10 @@ export class Store {
     }
     run() {
         // faster: 178 msecs
-        // this.data = []
-        // setTimeout(() => {this.data = this.buildData();});
+        this.data = []
+        setTimeout(() => {this.data = this.buildData();});
         // slower: 194 msecs
-        this.data.splice(0, this.data.length, ...this.buildData());
+        // this.data.splice(0, this.data.length, ...this.buildData());
     }
     add() {
         this.data = this.data.concat(this.buildData(1000));
