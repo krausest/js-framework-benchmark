d83be061eefa2e16292270bdca0eb281a4aae01d
Trotyl <trotyl@qq.com>
2018-07-26 22:29:17 +0800

    fix(angular): update folder name for keyed

--- webdriver-ts/src/common.ts
+++ webdriver-ts/src/common.ts
@@ -58,7 +58,7 @@ function f(name: string, keyed: boolean, options: Options = {uri: null, useShado
 export let frameworks = [
     f("angular-light-v0.14.1-keyed", true),
     f("angular-v1.6.3-keyed", true),
-    f("angular-v6.0.0-keyed", true),
+    f("angular-v6.1.0-keyed", true),
     f("angular-v6.0.0-non-keyed", false),
     f("angular-v6.0.0-no-zone-keyed", true),
     f("angular-v6.0.0-optimized-keyed", true),
