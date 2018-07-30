67152a6c0a6c25296261b99d118aae15c664d11a
Stefan Krause <Stefan.Krause@gmx.at>
2018-07-30 21:25:56 +0200

    update svelte

--- frameworks/non-keyed/svelte/rollup.config.js
+++ frameworks/non-keyed/svelte/rollup.config.js
@@ -1,6 +1,6 @@
 import svelte from 'rollup-plugin-svelte';
 import buble from 'rollup-plugin-buble';
-import uglify from 'rollup-plugin-uglify';
+import {uglify} from 'rollup-plugin-uglify';
 
 const plugins = [
     svelte(),
