492d00b4c3075cb9b557e44b118a3f7a8bd81ee7
Stefan Krause <Stefan.Krause@gmx.at>
2018-08-01 21:37:14 +0200

    added rebuild script

---
+++ rebuild.js
@@ -0,0 +1,41 @@
+var _ = require('lodash');
+var exec = require('child_process').execSync;
+var fs = require('fs');
+var path = require('path');
+var yargs =require('yargs');
+
+let args = yargs(process.argv)
+    .array("framework").argv;
+
+for (let framework of args.framework) {
+    let dir = 'frameworks/'+framework;
+    console.log("rebuilding "+framework);
+    if (!fs.existsSync(dir)) console.log ("ERROR: directory "+dir+" not found");
+    else {
+        console.log("running rm -rf package-lock.json yarn.lock dist elm-stuff bower_components node_modules");
+        try {
+            exec('rm -r package-lock.json yarn.lock dist elm-stuff bower_components node_modules', {
+                cwd: dir,
+                stdio: 'inherit'
+            });
+        } catch {}
+        console.log("running npm install && npm run build-prod");
+        exec('npm install && npm run build-prod', {
+            cwd: dir,
+            stdio: 'inherit'
+        });
+        exec('npm install && npm run build-prod', {
+            cwd: dir,
+            stdio: 'inherit'
+        });
+        }
+    exec('npm run index', {
+        cwd: 'webdriver-ts',
+        stdio: 'inherit'
+    });
+    let frameworkNames = args.framework.map(f => f.split("/")[1]).join(" ");
+    exec('npm run nonKeyed -- --framework '+frameworkNames, {
+        cwd: 'webdriver-ts',
+        stdio: 'inherit'
+    });
+}
