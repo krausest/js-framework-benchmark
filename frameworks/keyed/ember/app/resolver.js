d0a2a37a14052290e6735b88a89603014cc562ae
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-08-10 23:35:33 -0400

    I think I broke the data updating in my reafactor

--- frameworks/keyed/ember/app/resolver.js
+++ frameworks/keyed/ember/app/resolver.js
@@ -1,23 +1,5 @@
-import Resolver from 'ember-resolver/resolvers/fallback';
+import Resolver from 'ember-resolver';
+export default Resolver;
 
-import { merge } from '@ember/polyfills';
 
-import buildResolverConfig from 'ember-resolver/ember-config';
-import config from '../config/environment';
 
-let moduleConfig = buildResolverConfig(config.modulePrefix);
-/*
- * If your application has custom types and collections, modify moduleConfig here
- * to add support for them.
- */
-
-merge(moduleConfig.types, {
-  config: { definitiveCollection: 'main' },
-  util: { definitiveCollection: 'utils' },
-});
-
-moduleConfig.collections.main.types.push('config');
-
-export default Resolver.extend({
-  config: moduleConfig,
-});
