32415c1f4c98abefff99208b962d98c07cd08616
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-08-10 23:33:43 -0400

    forgot files

---
+++ frameworks/keyed/ember/tests/unit/utils/benchmark-helpers-test.js
@@ -0,0 +1,11 @@
+import benchmarkHelpers from 'ember-temp/utils/benchmark-helpers';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | benchmark-helpers', function(hooks) {
+
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = benchmarkHelpers();
+    assert.ok(result);
+  });
+});
