c4b5be2a7c1d4ad2c52411d242e5a8771c4bc743
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-08-10 22:41:29 -0400

    ugrpades, enable optional features

--- frameworks/keyed/ember/.eslintrc.js
+++ frameworks/keyed/ember/.eslintrc.js
@@ -20,8 +20,9 @@ module.exports = {
     // node files
     {
       files: [
-        'testem.js',
         'ember-cli-build.js',
+        'testem.js',
+        'blueprints/*/index.js',
         'config/**/*.js',
         'lib/*/index.js'
       ],
