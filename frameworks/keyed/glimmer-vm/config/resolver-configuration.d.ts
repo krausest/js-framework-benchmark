bc30462efff74506ac984b4376a5aa19748ac24b
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-11-10 15:36:07 -0500

    oops

--- frameworks/keyed/glimmer-vm/config/resolver-configuration.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This is just a placeholder file to keep TypeScript aware editors happy. At build time,
- * it will be replaced with a resolver configuration composed from your application's
- * `config/environment.js` (and supplemented with default settings as possible).
- */
-
-import { ResolverConfiguration } from '@glimmer/resolver';
-declare var _default: ResolverConfiguration;
-export default _default;
