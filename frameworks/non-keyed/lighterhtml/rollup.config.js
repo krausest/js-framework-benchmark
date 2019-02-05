a875f2730685dab7d0e44e04373daa440a06879f
Mathis Zeiher <mathis.zeiher@gmail.com>
2019-02-05 19:35:03 +0100

    improved rollup config to prevent warnings

--- frameworks/non-keyed/lighterhtml/rollup.config.js
+++ frameworks/non-keyed/lighterhtml/rollup.config.js
@@ -5,6 +5,8 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
     input: `src/index.js`,
+    context: 'null', 
+    moduleContext: 'null',
     output: {
         file: `dist/index.js`,
         format: 'iife'
