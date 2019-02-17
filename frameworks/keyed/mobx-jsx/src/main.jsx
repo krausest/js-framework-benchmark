5899762c4b8379e37817d5b93621acbd21273434
Ryan Carniato <ryansolid@gmail.com>
2019-02-16 20:15:40 -0800

    Update Solid, KoJSX, and MobXJSX v0.4.0

--- frameworks/keyed/mobx-jsx/src/main.jsx
+++ frameworks/keyed/mobx-jsx/src/main.jsx
@@ -22,22 +22,20 @@ function buildData(count) {
 }
 
 const Button = ({ id, text, fn }) =>
-  <div class ='col-sm-6 smallpad'>
+  <div class='col-sm-6 smallpad'>
     <button id={ id } class='btn btn-primary btn-block' type='button' onClick={ fn }>{ text }</button>
   </div>
 
 const App = () => {
   const state = observable({data: [], selected: null});
 
-  const clickRow = (e, id, intent) => {
-    action(() => {
-      if (intent === 'remove') {
-        const data = state.data.slice(0);
-        data.splice(data.findIndex(d => d.id === id), 1)
-        state.data = data;
-      } else state.selected = id;
-    })();
-  };
+  const remove = (e, id) => action(() => {
+    const data = state.data.slice(0);
+    data.splice(data.findIndex(d => d.id === id), 1)
+    state.data = data;
+  })();
+
+  const select = (e, id) => action(() => state.selected = id)();
 
   const run = action(e => {
     state.data = buildData(1000);
@@ -77,8 +75,8 @@ const App = () => {
 
   return <div class='container'>
     <div class='jumbotron'><div class='row'>
-      <div class ='col-md-6'><h1>MobX-JSX Keyed</h1></div>
-      <div class ='col-md-6'><div class ='row'>
+      <div class='col-md-6'><h1>MobX-JSX Keyed</h1></div>
+      <div class='col-md-6'><div class='row'>
         <Button id='run' text='Create 1,000 rows' fn={ run } />
         <Button id='runlots' text='Create 10,000 rows' fn={ runLots } />
         <Button id='add' text='Append 1,000 rows' fn={ add } />
@@ -87,15 +85,14 @@ const App = () => {
         <Button id='swaprows' text='Swap Rows' fn={ swapRows } />
       </div></div>
     </div></div>
-    <table class='table table-hover table-striped test-data'><tbody onClick={ clickRow }>
-      <$ each={ state.data } afterRender={selectWhen(() => state.selected, 'danger')}>{
-        row =>
-          <tr model={ row.id }>
-            <td class='col-md-1' textContent={ row.id } />
-            <td class='col-md-4'><a>{( row.label )}</a></td>
-            <td class='col-md-1'><a action={'remove'}><span class='glyphicon glyphicon-remove' /></a></td>
-            <td class='col-md-6'/>
-          </tr>
+    <table class='table table-hover table-striped test-data'><tbody>
+      <$ each={ state.data } afterRender={selectWhen(() => state.selected, 'danger')}>{ row =>
+        <tr model={ row.id }>
+          <td class='col-md-1' textContent={ row.id } />
+          <td class='col-md-4'><a onClick={ select }>{( row.label )}</a></td>
+          <td class='col-md-1'><a onClick={ remove }><span class='glyphicon glyphicon-remove' /></a></td>
+          <td class='col-md-6'/>
+        </tr>
       }</$>
     </tbody></table>
     <span class='preloadicon glyphicon glyphicon-remove' aria-hidden="true" />
