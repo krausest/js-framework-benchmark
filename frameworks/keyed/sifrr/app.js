973cec233885c1503de3080fc801b26b59a171f3
Stefan Krause <Stefan.Krause@gmx.at>
2019-03-14 20:33:46 +0100

    removed symlinks, updated results for sifrr

--- frameworks/keyed/sifrr/app.js
+++ frameworks/keyed/sifrr/app.js
@@ -1,1 +1,7 @@
-../../non-keyed/sifrr/app.js
+import SifrrDOM from '@sifrr/dom'
+import SifrrFetch from '@sifrr/fetch'
+
+window.Sifrr = { Dom: SifrrDOM, Fetch: SifrrFetch };
+
+Sifrr.Dom.setup({ baseUrl: '/frameworks/non-keyed/sifrr/' });
+Sifrr.Dom.Event.add('click');
--- frameworks/keyed/sifrr/rollup.config.js
+++ frameworks/keyed/sifrr/rollup.config.js
@@ -1,1 +1,53 @@
-../../non-keyed/sifrr/rollup.config.js
+'use strict';
+
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import { terser } from "rollup-plugin-terser";
+import commonjs from 'rollup-plugin-commonjs';
+
+export default [{
+  input: 'app.js',
+  output: {
+    file: 'dist/app.min.js',
+    format: 'iife',
+    name: 'app',
+    sourcemap: true,
+    globals: {
+      '@sifrr/dom': 'Sifrr.Dom',
+      '@sifrr/fetch': 'Sifrr.Fetch'
+    }
+  },
+  plugins: [
+    resolve({
+      browser: true
+    }),
+    commonjs(),
+    babel(),
+    terser({
+      keep_classnames: true
+    })
+  ],
+},{
+  input: 'elements.js',
+  output: {
+    file: 'dist/elements.min.js',
+    format: 'iife',
+    name: 'app',
+    sourcemap: true,
+    globals: {
+      '@sifrr/dom': 'Sifrr.Dom',
+      '@sifrr/fetch': 'Sifrr.Fetch'
+    }
+  },
+  external: [ '@sifrr/dom' ],
+  plugins: [
+    resolve({
+      browser: true
+    }),
+    commonjs(),
+    babel(),
+    terser({
+      keep_classnames: true
+    })
+  ],
+}]
