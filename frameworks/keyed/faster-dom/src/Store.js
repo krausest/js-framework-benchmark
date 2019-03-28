cdddacd478fafde03fc947883d71f41c5f6e66dc
Iurii Panarin <iurii.panarin@mcmakler.de>
2019-03-28 11:30:54 +0100

    feat(frameworks): add fastDom

---
+++ frameworks/keyed/faster-dom/src/Store.js
@@ -0,0 +1,121 @@
+import { fdValue } from 'faster-dom';
+
+let id = 1;
+
+const rand = Math.random
+
+function buildData(count) {
+    const adjectives = [
+        "pretty",
+        "large",
+        "big",
+        "small",
+        "tall",
+        "short",
+        "long",
+        "handsome",
+        "plain",
+        "quaint",
+        "clean",
+        "elegant",
+        "easy",
+        "angry",
+        "crazy",
+        "helpful",
+        "mushy",
+        "odd",
+        "unsightly",
+        "adorable",
+        "important",
+        "inexpensive",
+        "cheap",
+        "expensive",
+        "fancy",
+    ]
+
+    const colours = [
+        "red",
+        "yellow",
+        "blue",
+        "green",
+        "pink",
+        "brown",
+        "purple",
+        "brown",
+        "white",
+        "black",
+        "orange",
+    ]
+
+    const nouns = [
+        "table",
+        "chair",
+        "house",
+        "bbq",
+        "desk",
+        "car",
+        "pony",
+        "cookie",
+        "sandwich",
+        "burger",
+        "pizza",
+        "mouse",
+        "keyboard",
+    ]
+
+    return new Array(count).fill(0).map(_ => (fdValue({
+        id: id++,
+        label: `${adjectives[
+            rand() * 1000 % adjectives.length >> 0]} ${colours[
+            rand() * 1000 % colours.length >> 0]} ${nouns[
+            rand() * 1000 % nouns.length >> 0]}`
+    }, true)))
+}
+
+export class Store {
+    constructor() {
+        this.data = fdValue([]);
+        this.selectedId = fdValue(null)
+        this.select = (index) => {
+            this.selectedId.value = index;
+        }
+        this.remove = (removeIndex) => {
+            this.data.value = [...this.data.value.slice(0, removeIndex),
+            ...this.data.value.slice(removeIndex + 1)]
+        }
+    }
+
+    setData(count = 1000) {
+        this.data.value = buildData(count);
+    }
+
+    append(count = 1000) {
+        this.data.value = [...this.data.value, ...buildData(count)]
+    }
+
+    clear() {
+        this.data.value = [];
+    }
+
+    update() {
+        const length = this.data.value.length
+        for (let i = 0; i < length; i += 10) {
+            const item = this.data.value[i]
+            item.value = Object.assign(item.value, {
+                label: `${item.value.label} !!!`
+            })
+        }
+    }
+
+    swapData() {
+        const arr = this.data.value
+        if (arr.length <= 998) {
+            return
+        }
+
+        const temp = arr[1]
+        arr[1] = arr[998]
+        arr[998] = temp
+        this.data.value = [...arr]
+    }
+}
---
+++ frameworks/non-keyed/faster-dom/src/Main.js
@@ -0,0 +1,333 @@
+import { Component, createComponent, fdFor, fdObject, fdValue, generateNode } from 'faster-dom';
+
+import { Store } from './Store';
+
+let startTime;
+let lastMeasure;
+const startMeasure = (name) => {
+    startTime = performance.now();
+    lastMeasure = name;
+}
+const stopMeasure = () => {
+    let last = lastMeasure;
+    if (lastMeasure) {
+        window.setTimeout(() => {
+            lastMeasure = null;
+            const stop = performance.now();
+            console.log(last + " took " + (stop - startTime));
+        }, 0);
+    }
+}
+
+function createTr(input) {
+    return createComponent(TrComponent, input)
+}
+
+class TrComponent extends Component {
+    constructor(inputs) {
+        super();
+        this.item = inputs.item;
+        const index = inputs.index
+        const idObs = fdValue(this.item.value.id)
+        const labelObs = fdValue(this.item.value.label)
+        this.selectedIdObs = inputs.selectedId;
+        const selectedObs = fdValue(this.selectedIdObs.value === index ? true : false)
+
+
+        this.selectedSub = (newIndex) => {
+            selectedObs.value = newIndex === index ? true : false
+        }
+        this.selectedIdObs.addSubscriber(this.selectedSub)
+        this.subscriber = (newItem) => {
+            idObs.value = newItem.id
+            labelObs.value = newItem.label
+        }
+
+        this.item.addSubscriber(this.subscriber)
+        this.reactive = {
+            selected: selectedObs
+        }
+        this.fdObjects = {
+            trClasses: new fdObject({
+                'danger': this.reactive.selected
+            }),
+        }
+        this.onSelectClick = () => {
+            startMeasure('select')
+            inputs.onSelect(index);
+            stopMeasure();
+        }
+        this.onRemoveClick = () => {
+            startMeasure('remove')
+            inputs.onRemove(index);
+            stopMeasure();
+        }
+        this.template = {
+            tag: "tr",
+            classList: this.fdObjects.trClasses,
+            children: [
+                {
+                    tag: "td",
+                    classList: ['col-md-1'],
+                    textValue: idObs,
+                },
+                {
+                    tag: "td",
+                    classList: ['col-md-4'],
+                    children: [
+                        {
+                            tag: "a",
+                            attrs: {
+                                'data-action': 'select',
+                            },
+                            listeners: {
+                                click: this.onSelectClick
+                            },
+                            textValue: labelObs,
+                        }
+                    ]
+                },
+                {
+                    tag: "td",
+                    classList: ['col-md-1'],
+                    children: [
+                        {
+                            tag: 'a',
+                            children: [
+                                {
+                                    tag: 'span',
+                                    classList: ['remove', 'glyphicon', 'glyphicon-remove'],
+                                    listeners: {
+                                        click: this.onRemoveClick
+                                    },
+                                    attrs: {
+                                        'aria-hidden': 'true'
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+
+    onDestroy() {
+        this.item.removeSubscriber(this.subscriber)
+        this.selectedIdObs.removeSubscriber(this.selectedSub);
+    }
+}
+
+function createMainContainer() {
+    return createComponent(MainContainer);
+}
+
+class MainContainer extends Component {
+    constructor() {
+        super();
+        this.store = new Store();
+        this.onRunClick = () => {
+            startMeasure("run");
+            this.store.clear();
+            this.store.setData();
+            stopMeasure();
+        }
+        this.onRunLotsClick = () => {
+            startMeasure("runLots");
+            this.store.setData(10000);
+            stopMeasure();
+        }
+        this.onAppendClick = () => {
+            startMeasure("add");
+            this.store.append();
+            stopMeasure();
+        }
+        this.onClear = () => {
+            startMeasure("clear");
+            this.store.clear();
+            stopMeasure();
+        }
+        this.onUpdateClick = () => {
+            startMeasure("update");
+            this.store.update();
+            stopMeasure();
+        }
+        this.onSwapClick = () => {
+            startMeasure("swapRows");
+            this.store.swapData();
+            stopMeasure();
+        }
+        this.template = {
+            tag: "div",
+            classList: ["container"],
+            children: [
+                {
+                    tag: "div",
+                    classList: ["jumbotron"],
+                    children: [
+                        {
+                            tag: "div",
+                            classList: ['row'],
+                            children: [
+                                {
+                                    tag: "div",
+                                    classList: ["col-md-6"],
+                                    children: [
+                                        {
+                                            tag: "h1",
+                                            textValue: 'FastDom-"not-keyed""'
+                                        }
+                                    ]
+                                },
+                                {
+                                    tag: "div",
+                                    classList: ['col-md-6'],
+                                    children: [
+                                        {
+                                            tag: "div",
+                                            classList: ['row'],
+                                            children: [
+                                                {
+                                                    tag: "div",
+                                                    classList: ['col-sm-6', 'smallpad'],
+                                                    children: [
+                                                        {
+                                                            tag: "button",
+                                                            classList: ['btn', 'btn-primary', 'btn-block'],
+                                                            attrs: {
+                                                                id: "run"
+                                                            },
+                                                            textValue: "Create 1,000 rows",
+                                                            listeners: {
+                                                                click: this.onRunClick,
+                                                            }
+                                                        }
+                                                    ]
+                                                },
+                                                {
+                                                    tag: "div",
+                                                    classList: ['col-sm-6', 'smallpad'],
+                                                    children: [
+                                                        {
+                                                            tag: "button",
+                                                            attrs: {
+                                                                id: "runlots"
+                                                            },
+                                                            classList: ['btn', 'btn-primary', 'btn-block'],
+                                                            textValue: "Create 10,000 rows",
+                                                            listeners: {
+                                                                click: this.onRunLotsClick,
+                                                            }
+                                                        }
+                                                    ]
+                                                },
+                                                {
+                                                    tag: "div",
+                                                    classList: ['col-sm-6', 'smallpad'],
+                                                    children: [
+                                                        {
+                                                            tag: "button",
+                                                            attrs: {
+                                                                id: "add"
+                                                            },
+                                                            classList: ['btn', 'btn-primary', 'btn-block'],
+                                                            textValue: "Append 1,000 rows",
+                                                            listeners: {
+                                                                click: this.onAppendClick,
+                                                            }
+                                                        }
+                                                    ]
+                                                },
+                                                {
+                                                    tag: "div",
+                                                    classList: ['col-sm-6', 'smallpad'],
+                                                    children: [
+                                                        {
+                                                            tag: "button",
+                                                            attrs: {
+                                                                id: "update"
+                                                            },
+                                                            classList: ['btn', 'btn-primary', 'btn-block'],
+                                                            textValue: "Update every 10th row",
+                                                            listeners: {
+                                                                click: this.onUpdateClick,
+                                                            }
+                                                        }
+                                                    ]
+                                                },
+                                                {
+                                                    tag: "div",
+                                                    classList: ['col-sm-6', 'smallpad'],
+                                                    children: [
+                                                        {
+                                                            tag: "button",
+                                                            attrs: {
+                                                                id: "clear"
+                                                            },
+                                                            classList: ['btn', 'btn-primary', 'btn-block'],
+                                                            textValue: "Clear",
+                                                            listeners: {
+                                                                click: this.onClear,
+                                                            }
+                                                        }
+                                                    ]
+                                                },
+                                                {
+                                                    tag: "div",
+                                                    classList: ['col-sm-6', 'smallpad'],
+                                                    children: [
+                                                        {
+                                                            tag: "button",
+                                                            attrs: {
+                                                                id: "swaprows"
+                                                            },
+                                                            classList: ['btn', 'btn-primary', 'btn-block'],
+                                                            textValue: "Swap Rows",
+                                                            listeners: {
+                                                                click: this.onSwapClick,
+                                                            }
+                                                        }
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    tag: "table",
+                    attrs: {
+                        id: "table"
+                    },
+                    classList: ['table', 'table-hover', 'table-striped', 'test-data'],
+                    children: [
+                        {
+                            tag: "tbody",
+                            children: [
+                                fdFor(this.store.data, createTr, {
+                                    item: (e) => e,
+                                    selectedId: () => this.store.selectedId,
+                                    index: (e, index) => index,
+                                    onSelect: () => this.store.select,
+                                    onRemove: () => this.store.remove
+                                })
+                            ]
+                        }
+                    ]
+                },
+                {
+                    tag: "span",
+                    attrs: {
+                        'aria-hidden': true,
+                    },
+                    classList: ['preloadicon', 'glyphicon', 'glyphicon-remove']
+                }
+            ]
+        }
+    }
+}
+
+document.getElementById('main').appendChild(generateNode(createMainContainer()))
