5543708b760e071ee9ec4f22720cc6f1317e79c0
Michael Boulton <michaelboulton@gmail.com>
2019-05-22 21:30:46 +0100

    lockfiles for new esy.json

--- frameworks/non-keyed/incr_dom/run.sh
+++ frameworks/non-keyed/incr_dom/run.sh
@@ -3,7 +3,7 @@
 set -ex
 
 pkill -f /home/michael/.esy/3__________________________________________________________________/i/opam__s__js__of__ocaml_compiler-opam || true;
-esy install
-# esy b refmterr dune build --root . -j 3 #--verbose
-esy b refmterr dune build --root . -j 3 --verbose --profile release
-# esy run
+esy @esy install
+# esy @esy b refmterr dune build --root . -j 3 #--verbose
+esy @esy b refmterr dune build --root . -j 3 --verbose --profile release
+# esy @esy run
