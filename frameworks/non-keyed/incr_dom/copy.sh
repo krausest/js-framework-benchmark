65ce2d95d58f51dd741de1dfc01436c7edc888ef
Michael Boulton <michaelboulton@gmail.com>
2019-05-25 12:46:20 +0100

    Remove cerbosity in building dev

--- frameworks/non-keyed/incr_dom/copy.sh
+++ frameworks/non-keyed/incr_dom/copy.sh
@@ -1,6 +1,11 @@
+#!/bin/sh
+
+set -ex
+
 RELEASE_PATH="$(pwd)/dist"
 mkdir -p $RELEASE_PATH
 
 built=$(find | grep .bc.js | grep src)
 
+rm -f $RELEASE_PATH/*.bc.js
 cp $built $RELEASE_PATH
--- frameworks/non-keyed/incr_dom/run.sh
+++ frameworks/non-keyed/incr_dom/run.sh
@@ -3,7 +3,8 @@
 set -ex
 
 pkill -f /home/michael/.esy/3__________________________________________________________________/i/opam__s__js__of__ocaml_compiler-opam || true;
-esy @esy install
+# esy @esy install
 # esy @esy b refmterr dune build --root . -j 3 #--verbose
-esy @esy b refmterr dune build --root . -j 3 --verbose --profile release
+# esy @esy b refmterr dune build --root . -j 3 --verbose --profile release
 # esy @esy run
+npm run build-dev
