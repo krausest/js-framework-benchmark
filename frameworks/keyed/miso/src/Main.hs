21ebad7d410ce9a05141f2fd4cb84908afae4014
David Johnson <djohnson.m@gmail.com>
2019-07-21 23:58:42 -0400

    Bump miso to 1.1.0.0

--- frameworks/keyed/miso/src/Main.hs
+++ frameworks/keyed/miso/src/Main.hs
@@ -232,7 +232,7 @@ viewJumbotron =
         [ class_ "col-md-6" ]
         [ h1_
             []
-            [ text "miso-0.18.0.0-keyed" ]
+            [ text "miso-1.1.0.0-keyed" ]
         ]
       , div_
           [ class_ "col-md-6" ]
