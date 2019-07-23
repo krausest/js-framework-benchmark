eb57792c1c01299463b472a9a09ef0cc439df0a0
David Johnson <djohnson.m@gmail.com>
2019-07-23 11:06:10 -0400

    Add non-keyed implementation.

---
+++ frameworks/non-keyed/miso/default.nix
@@ -0,0 +1,19 @@
+with (import (builtins.fetchTarball {
+  url = "https://github.com/dmjio/miso/archive/5647cfd.tar.gz";
+  sha256 = "177d99m4q4ab35xr0kdpczxncbx187bwk54z7cr3khp5w9gcq27g";
+}) {});
+with pkgs.haskell.packages;
+let
+  app = ghcjs.callCabal2nix "miso-benchmark-non-keyed" ./. {};
+in
+  pkgs.lib.overrideDerivation app (drv: {
+    postInstall = with pkgs; ''
+      mkdir -p $out/bin
+      ${closurecompiler}/bin/closure-compiler --compilation_level ADVANCED_OPTIMIZATIONS \
+          --jscomp_off=checkVars \
+          --externs=$out/bin/miso-benchmark-non-keyed.jsexe/all.js.externs \
+          $out/bin/miso-benchmark-non-keyed.jsexe/all.js > temp.js
+          mv temp.js $out/bin/all.min.js
+          rm -r $out/bin/miso-benchmark-non-keyed.jsexe
+      '';
+  })
