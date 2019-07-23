239c8977e8e9340e83d066b51ed8028eb79f8933
David Johnson <djohnson.m@gmail.com>
2019-07-23 15:35:00 -0400

    echo "(window['gc'] = window['gc']);" >> $out/bin/miso-benchmark-keyed.jsexe/all.js

--- frameworks/non-keyed/miso/default.nix
+++ frameworks/non-keyed/miso/default.nix
@@ -9,6 +9,7 @@ in
   pkgs.lib.overrideDerivation app (drv: {
     postInstall = with pkgs; ''
       mkdir -p $out/bin
+      echo "(window['gc'] = window['gc']);" >> $out/bin/miso-benchmark-non-keyed.jsexe/all.js
       ${closurecompiler}/bin/closure-compiler --compilation_level ADVANCED_OPTIMIZATIONS \
           --jscomp_off=checkVars \
           --externs=$out/bin/miso-benchmark-non-keyed.jsexe/all.js.externs \
