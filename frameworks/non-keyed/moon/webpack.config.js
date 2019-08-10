a6eaae50fdf6c87e834ba7587b1cf88f7030e684
Kabir Shah <iamkbrsh@gmail.com>
2019-08-09 23:59:51 -0700

    update config and remove extra template

--- frameworks/non-keyed/moon/webpack.config.js
+++ frameworks/non-keyed/moon/webpack.config.js
@@ -1,7 +1,10 @@
+const path = require("path");
+
 module.exports = {
 	entry: "./src/index.js",
 	output: {
-		filename: "bundle.js"
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist")
 	},
 	module: {
 		rules: [
