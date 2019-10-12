d42c8029cc32772fddb4f6edd64ac01a1419aebf
Thomas Wilkerling <thomas@nextapps.de>
2019-10-12 16:29:09 +0200

    Update Mikado to v0.6.5

---
+++ frameworks/non-keyed/mikado/src/config.js
@@ -0,0 +1,83 @@
+/**
+ * @define {boolean}
+ */
+
+const DEBUG = true;
+
+/**
+ * @define {boolean}
+ */
+
+const USE_POLYFILL = false;
+
+/**
+ * @define {boolean}
+ */
+
+const SUPPORT_CACHE = false;
+
+/**
+ * @define {boolean}
+ */
+
+const SUPPORT_EVENTS = true;
+
+/**
+ * @define {boolean}
+ */
+
+const SUPPORT_STORAGE = true;
+
+/**
+ * @define {boolean|string}
+ */
+
+const SUPPORT_HELPERS = "swap";
+
+/**
+ * @define {boolean}
+ */
+
+const SUPPORT_CACHE_HELPERS = false;
+
+/**
+ * @define {boolean}
+ */
+
+const SUPPORT_POOLS = false;
+
+/**
+ * @define {boolean}
+ */
+
+const SUPPORT_ASYNC = false;
+
+/**
+ * @define {boolean}
+ */
+
+const SUPPORT_TRANSPORT = false;
+
+/**
+ * @define {boolean}
+ */
+
+const SUPPORT_TEMPLATE_EXTENSION = false;
+
+/**
+ * @define {boolean}
+ */
+
+const SUPPORT_REACTIVE = false;
+
+/**
+ * @define {boolean}
+ */
+
+const SUPPORT_COMPILE = false;
+
+/**
+ * @define {string}
+ */
+
+const RELEASE = "custom";
