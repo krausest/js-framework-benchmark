67f4601809329959789ec3d23deb27bf0994d193
Rango Yuan <yuanoook@gmail.com>
2019-11-25 11:51:25 +0800

    update ganic benchmark title

--- frameworks/keyed/ganic/src/Benchmark.js
+++ frameworks/keyed/ganic/src/Benchmark.js
@@ -27,7 +27,7 @@ const Jumbotron = ({ dispatch }) => (
   <div class="jumbotron">
     <div class="row">
       <div class="col-md-6">
-        <h1>Ganic UseX keyed</h1>
+        <h1>Ganic keyed</h1>
       </div>
       <div class="col-md-6">
         <div class="row">
