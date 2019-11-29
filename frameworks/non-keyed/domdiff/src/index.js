bc8624055ff658bc653a90a4dedcc3f4f03c858c
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-11-29 10:47:28 +0100

    domdiff: final test-case update

--- frameworks/non-keyed/domdiff/src/index.js
+++ frameworks/non-keyed/domdiff/src/index.js
@@ -1,20 +1,21 @@
 import domdiff from 'domdiff';
-import {Scope, getRow, spliceRows} from './utils.js';
+import {State} from 'js-framework-benchmark-utils';
+import {getRow, spliceRows} from './utils.js';
 
 const tbody = document.querySelector('tbody');
 let rows = [].slice.call(tbody.children);
-const scope = new Scope(({data, selected}) => {
+const state = State(({data, selected, select, remove}) => {
   rows = domdiff(
     tbody,
     rows,
     data.map((item, index) => {
-      const info = getRow(scope, item, index);
-      const {row, select, td} = info;
       const {id, label} = item;
+      const info = getRow(index, select, remove, id, label);
+      const {row, selector, td} = info;
       if (info.id !== id)
         td.textContent = (row.id = (info.id = id));
       if (info.label !== label)
-        select.textContent = (info.label = label);
+        selector.textContent = (info.label = label);
       const danger = id === selected;
       if (info.danger !== danger)
         row.classList.toggle('danger', (info.danger = danger));
@@ -24,8 +25,8 @@ const scope = new Scope(({data, selected}) => {
   spliceRows(rows.length);
 });
 
-Object.keys(scope).forEach(id => {
+Object.keys(state).forEach(id => {
   const button = document.querySelector(`#${id.toLowerCase()}`);
   if (button)
-    button.addEventListener('click', () => scope[id]());
+    button.addEventListener('click', () => state[id]());
 });
