90f1e35cc086047bba6b88702a3059f2d06c52fb
David Johnson <djohnson.m@gmail.com>
2020-02-05 23:54:51 -0500

    Update miso to 1.4.

--- frameworks/keyed/miso/default.nix
+++ frameworks/keyed/miso/default.nix
@@ -1,6 +1,6 @@
 with (import (builtins.fetchTarball {
-  url = "https://github.com/dmjio/miso/archive/485b91f.tar.gz";
-  sha256 = "1kfr1f6bwfqnvxlfzf1vv0v07xrlg8ashjiasrps85l1dmja6s4b";
+  url = "https://github.com/dmjio/miso/archive/d10c2d5a0def76b91d55abe3331370fc528b6d02.tar.gz";
+  sha256 = "0733f3i73j6aig2bdar1dfm8h806ghhqqcm1mx7sy741cdvv77n3";
 }) {});
 with pkgs.haskell.packages;
 let
