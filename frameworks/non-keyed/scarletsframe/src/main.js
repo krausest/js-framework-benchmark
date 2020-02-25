bb0fe2d28840b5173488a901fc555d09ad7f980b
StefansArya <stefansarya1@gmail.com>
2020-02-25 21:56:40 +0700

    Update to version 0.25.1

--- frameworks/non-keyed/scarletsframe/src/main.js
+++ frameworks/non-keyed/scarletsframe/src/main.js
@@ -1,160 +1,90 @@
-var sf = require('scarletsframe');
-var $ = sf.dom;
+var sf = window.sf = require('scarletsframe');
 
 // Declare variable for the model
 sf.model.for('bench-mark', function(self){
     self.list = [];
-    self.selected = -1;
-    // window.test = self;
+    self.selectedItem = null;
+
+    // Handle button
+    self.b_run = function(){
+        self.list.assign(Store.buildData());
+        self.selectedItem = null;
+    }
+
+    self.b_runlots = function(){
+        self.list.assign(Store.buildData(10000));
+        self.selectedItem = null;
+    }
+
+    self.b_add = function(){
+        self.list.assign(self.list.concat(Store.buildData(1000)));
+    }
+
+    self.b_update = function(){
+        for (var i = 0; i < self.list.length; i += 10) {
+            self.list[i].label += ' !!!';
+        }
+    }
+
+    self.b_clear = function(){
+        self.list.splice(0);
+        self.selectedItem = null;
+    }
+
+    self.b_swaprows = function(){
+    	// Copy the object value
+        var ref = Object.assign({}, self.list[1]);
+
+        // Swap values between object
+        self.list.assign(1, self.list[998]);
+        self.list.assign(998, ref);
+    }
+
+    self.b_select = function(item){
+        // Reset last item selection
+        if(self.selectedItem !== item && self.selectedItem !== null)
+            self.selectedItem.selected = false;
+
+        // Select current item
+        item.selected = truNe;
+        self.selectedItem = item;
+    }
+
+    self.b_remove = function(item){
+        // Find item index from the list
+        var i = self.list.indexOf(item);
+
+        // Invalidate the selected item
+        if(self.selectedItem === item)
+            self.selectedItem = null;
+
+        // Remove item and the element
+        if(i !== -1)
+            self.list.splice(i, 1);
+    }
 });
 
-// Declare functions that controlling the model
-sf.controller.run('bench-mark', function(self, root){
-    var Measurer = root('measurer');
 
+// ==== Other stuff ====
+var Store = new function(){
     var adjectives = ["pretty", "large", "big", "small", "tall", "short", "long", "handsome", "plain", "quaint", "clean", "elegant", "easy", "angry", "crazy", "helpful", "mushy", "odd", "unsightly", "adorable", "important", "inexpensive", "cheap", "expensive", "fancy"];
     var colours = ["red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black", "orange"];
     var nouns = ["table", "chair", "house", "bbq", "desk", "car", "pony", "cookie", "sandwich", "burger", "pizza", "mouse", "keyboard"];
-
-    var nextId = 1;
 
     function _random(max) {
         return Math.round(Math.random()*1000)%max;
     }
 
-    self.buildData = function(count = 1000){
+    var nextId = 1;
+    this.buildData = function(count = 1000){
         var data = [];
         for (var i = 0; i < count; i++)
             data.push({
                 id: nextId++,
-                isSelected:false,
+                selected:'',
                 label: adjectives[_random(adjectives.length)] + " " + colours[_random(colours.length)] + " " + nouns[_random(nouns.length)]
             });
 
         return data;
     }
-
-    self.unselect = function(){
-        if(self.selected === -1) return;
-
-        if(self.list[self.selected] !== undefined){
-            self.list[self.selected].isSelected = false;
-            self.list.refresh(self.selected);
-        }
-
-        self.selected = -1;
-    }
-
-    // Handle button
-    self.b_run = function(){
-        // Measurer.start("run");
-        self.list = self.buildData();
-        self.selected = -1;
-        // Measurer.stop();
-    }
-
-    self.b_runlots = function(){
-        // Measurer.start("runLots");
-        self.list = self.buildData(10000);
-        self.selected = -1;
-        // Measurer.stop();
-    }
-
-    self.b_add = function(){
-        // Measurer.start("add");
-        self.list = self.list.concat(self.buildData(1000));
-        // Measurer.stop();
-    }
-
-    self.b_update = function(){
-        // Measurer.start("update");
-        for (var i = 0; i < self.list.length; i += 10) {
-            self.list[i].label += ' !!!';
-            self.list.refresh(i, 'label');
-        }
-        // Measurer.stop();
-    }
-
-    self.b_clear = function(){
-        // Measurer.start("clear");
-        self.list.splice(0);
-        self.selected = -1;
-        // Measurer.stop();
-    }
-
-    self.b_swaprows = function(){
-        // Measurer.start("swapRows");
-
-        if(self.list.length > 998)
-            self.list.swap(1, 998);
-
-        // Measurer.stop();
-    }
-
-    self.b_select = function(el){
-        // Measurer.start("select");
-        self.unselect();
-
-        var rowIndex = $.parent(el, '[sf-bind-list]');
-        self.selected = rowIndex = sf.model.index(rowIndex);
-
-        self.list[rowIndex].isSelected = true;
-        self.list.refresh(rowIndex);
-        // Measurer.stop();
-    }
-
-    self.b_remove = function(el){
-        // Measurer.start("delete");
-
-        var rowIndex = $.parent(el, '[sf-bind-list]');
-        rowIndex = sf.model.index(rowIndex);
-
-        self.list.splice(rowIndex, 1);
-
-        if(rowIndex === self.selected)
-            self.selected = -1;
-
-        // Measurer.stop();
-    }
-});
-
-// Declare measure function in different scope
-sf.controller.run('measurer', function(self){
-    var startTime;
-    var lastMeasure;
-
-    self.start = function(name) {
-        startTime = performance.now();
-        lastMeasure = name;
-    }
-    self.stop = function() {
-        var last = lastMeasure;
-        if (lastMeasure) {
-            window.setTimeout(function () {
-                lastMeasure = null;
-                var stop = performance.now();
-                var duration = 0;
-                console.log(last+" took "+(stop-startTime));
-            }, 0);
-        }
-    }
-});
-
-// Fix for (https://github.com/krausest/js-framework-benchmark/pull/519#issuecomment-464855788)
-sf(function(){
-    tbody.innerHTML = 
-    `<tr sf-repeat-this="x in list" class="{{ x.isSelected ? 'danger' : '' }}">
-        <td class="col-md-1">{{ x.id }}</td>
-        <td class="col-md-4">
-            <a class="lbl" sf-click="b_select(this)">{{ x.label }}</a>
-        </td>
-        <td class="col-md-1">
-         <a class="remove" sf-click="b_remove(this)">
-           <span class="remove glyphicon glyphicon-remove" aria-hidden="true"></span>
-         </a>
-        </td>
-        <td class="col-md-6"></td>
-    </tr>`;
-
-    sf.model.init(tbody);
-});
+};
