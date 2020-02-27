10b73ec51dd38e894bf5a873dfa9793d72f544bf
Jim Franke <info@franke.app>
2020-02-27 15:37:29 +0100

    Refactor: more inlining of code

--- frameworks/keyed/hyperapp/src/index.js
+++ frameworks/keyed/hyperapp/src/index.js
@@ -26,96 +26,94 @@ const LazyRow = ({ data, label, styleClass }) => (
   />
 );
 
-const view = state => (
-  <div class="container">
-    <div class="jumbotron">
-      <div class="row">
-        <div class="col-md-6">
-          <h1>HyperApp</h1>
-        </div>
-        <div class="col-md-6">
-          <div class="row">
-            <div class="col-sm-6 smallpad">
-              <button
-                type="button"
-                class="btn btn-primary btn-block"
-                id="run"
-                onclick={actions.run}
-              >
-                Create 1,000 rows
-              </button>
-            </div>
-            <div class="col-sm-6 smallpad">
-              <button
-                type="button"
-                class="btn btn-primary btn-block"
-                id="runlots"
-                onclick={actions.runLots}
-              >
-                Create 10,000 rows
-              </button>
-            </div>
-            <div class="col-sm-6 smallpad">
-              <button
-                type="button"
-                class="btn btn-primary btn-block"
-                id="add"
-                onclick={actions.add}
-              >
-                Append 1,000 rows
-              </button>
-            </div>
-            <div class="col-sm-6 smallpad">
-              <button
-                type="button"
-                class="btn btn-primary btn-block"
-                id="update"
-                onclick={actions.update}
-              >
-                Update every 10th row
-              </button>
-            </div>
-            <div class="col-sm-6 smallpad">
-              <button
-                type="button"
-                class="btn btn-primary btn-block"
-                id="clear"
-                onclick={actions.clear}
-              >
-                Clear
-              </button>
-            </div>
-            <div class="col-sm-6 smallpad">
-              <button
-                type="button"
-                class="btn btn-primary btn-block"
-                id="swaprows"
-                onclick={actions.swapRows}
-              >
-                Swap Rows
-              </button>
+app({
+  init: state,
+  view: state => (
+    <div class="container">
+      <div class="jumbotron">
+        <div class="row">
+          <div class="col-md-6">
+            <h1>HyperApp</h1>
+          </div>
+          <div class="col-md-6">
+            <div class="row">
+              <div class="col-sm-6 smallpad">
+                <button
+                  type="button"
+                  class="btn btn-primary btn-block"
+                  id="run"
+                  onclick={actions.run}
+                >
+                  Create 1,000 rows
+                </button>
+              </div>
+              <div class="col-sm-6 smallpad">
+                <button
+                  type="button"
+                  class="btn btn-primary btn-block"
+                  id="runlots"
+                  onclick={actions.runLots}
+                >
+                  Create 10,000 rows
+                </button>
+              </div>
+              <div class="col-sm-6 smallpad">
+                <button
+                  type="button"
+                  class="btn btn-primary btn-block"
+                  id="add"
+                  onclick={actions.add}
+                >
+                  Append 1,000 rows
+                </button>
+              </div>
+              <div class="col-sm-6 smallpad">
+                <button
+                  type="button"
+                  class="btn btn-primary btn-block"
+                  id="update"
+                  onclick={actions.update}
+                >
+                  Update every 10th row
+                </button>
+              </div>
+              <div class="col-sm-6 smallpad">
+                <button
+                  type="button"
+                  class="btn btn-primary btn-block"
+                  id="clear"
+                  onclick={actions.clear}
+                >
+                  Clear
+                </button>
+              </div>
+              <div class="col-sm-6 smallpad">
+                <button
+                  type="button"
+                  class="btn btn-primary btn-block"
+                  id="swaprows"
+                  onclick={actions.swapRows}
+                >
+                  Swap Rows
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <table class="table table-hover table-striped test-data">
+        <tbody>
+          {state.data.map(data => (
+            <LazyRow
+              data={data}
+              label={data.label}
+              styleClass={data.id === state.selected ? 'danger' : ''}
+            />
+          ))}
+        </tbody>
+      </table>
+      <span class="preloadicon glyphicon glyphicon-remove" aria-hidden="true" />
     </div>
-    <table class="table table-hover table-striped test-data">
-      <tbody>
-        {state.data.map(data => (
-          <LazyRow
-            data={data}
-            label={data.label}
-            styleClass={data.id === state.selected ? 'danger' : ''}
-          />
-        ))}
-      </tbody>
-    </table>
-    <span class="preloadicon glyphicon glyphicon-remove" aria-hidden="true" />
-  </div>
-);
-
-app({
-  init: state,
-  view,
-  node: document.getElementById("main")
+  ),
+  node: document.getElementById('main')
 });
