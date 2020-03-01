e8feb9b8189e3773ef5bb8a1a203963c74148409
Michael Boulton <michaelboulton@gmail.com>
2020-03-01 15:37:10 +0000

    copy map if it's there

--- frameworks/non-keyed/incr_dom/copy.sh
+++ frameworks/non-keyed/incr_dom/copy.sh
@@ -1,11 +1,15 @@
-#!/bin/sh
-
-set -ex
+#!/bin/sh -x
+set -x
 
 RELEASE_PATH="$(pwd)/dist"
 mkdir -p $RELEASE_PATH
 
 built=$(find | grep .bc.js | grep src)
-
 rm -f $RELEASE_PATH/*.bc.js
 cp $built $RELEASE_PATH
+
+built=$(find | grep .bc.map | grep src)
+if [ ! -z "$build" ]; then
+  rm -f $RELEASE_PATH/*.bc.map
+  cp $built $RELEASE_PATH
+fi
