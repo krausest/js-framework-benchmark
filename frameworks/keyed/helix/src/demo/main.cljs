51f9fe4be3670a3091ff427a160715dfe814894d
Stefan Krause <stefan.krause@gmx.at>
2020-05-02 22:05:20 +0200

    fixed on-select and clean up

--- frameworks/keyed/helix/src/demo/main.cljs
+++ frameworks/keyed/helix/src/demo/main.cljs
@@ -5,29 +5,12 @@
             ["react-dom" :as rdom]
             [demo.utils :as u]))
 
-(def start-time (atom nil))
-(def last-measure (atom nil))
-
-(defn start-measure [name]
-  (reset! start-time (.now js/performance))
-  (reset! last-measure name))
-
-(defn stop-measure []
-  (if-let [last @last-measure]
-    (.setTimeout js/window
-                 (fn []
-                   (reset! last-measure nil)
-                   (let [stop (.now js/performance)]
-                     (.log js/console (str last " took " (- stop @start-time)))))
-                 0)))
-
-
-(defnc row [{:keys [data selected? on-click on-delete]}]
+(defnc row [{:keys [data selected? on-select on-delete]}]
   (d/tr {:class (if selected? "danger")}
     (d/td {:class "col-md-1"}
           (:id data))
     (d/td {:class "col-md-4"}
-          (d/a {:on-click (fn [e] (on-click (:id data)))}
+          (d/a {:on-click (fn [e] (on-select (:id data)))}
                (:label data)))
     (d/td {:class "col-md-1"}
      (d/a {:on-click (fn [e] (on-delete (:id data)))}
@@ -41,46 +24,33 @@
   (js/console.log "re-render")
   (let [[data set-data] (use-state [])
         [selected set-selected] (use-state nil)
-        print-duration
-        (fn print-duration []
-          (stop-measure))
         run
         (fn run [_]
-          (start-measure "run")
           (set-data (vec (u/build-data id-atom 1000)))
           (set-selected nil))
         run-lots
         (fn run-lots [_]
-          (start-measure "runLots")
           (set-data (vec (u/build-data id-atom 10000)))
           (set-selected nil))
         add
         (fn add [_]
-          (start-measure "add")
           (set-data u/add id-atom))
         update-some
         (fn update-some []
-          (start-measure "update")
           (set-data u/update-some))
         clear
         (fn clear []
-          (start-measure "clear")
           (set-selected nil)
           (set-data []))
         swap-rows
         (fn swap-rows []
-          (start-measure "swapRows")
           (set-data u/swap-rows))
         select
         (fn select [id]
-          (start-measure "select")
           (set-selected id))
         delete
         (fn delete [id]
-          (start-measure "delete")
           (set-data u/delete-row id))]
-    (use-effect :always
-      (print-duration))
     (d/div
      {:class "container"}
      (d/div
@@ -88,7 +58,7 @@
       (d/div
        {:class "row"}
        (d/div {:class "col-md-6"}
-              (d/h1 (str "Helix (total rows " (count data) ")")))
+              (d/h1 (str "Helix")))
        (d/div {:class "col-md-6"}
               (d/div {:class "row"}
                      (d/div {:class "col-sm-6 smallpad"}
