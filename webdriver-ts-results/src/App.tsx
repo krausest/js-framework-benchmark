c922ef25dc8779e9dd35b3613108b195e464b93c
Stefan Krause <stefan.krause@gmx.at>
2020-08-09 22:07:37 +0200

    Update OS

--- webdriver-ts-results/src/App.tsx
+++ webdriver-ts-results/src/App.tsx
@@ -189,7 +189,7 @@ class App extends React.Component<{}, State> {
     return (
       <div>
         {disclaimer}
-        <p>The benchmark was run on a Razer Blade 15 Advanced (i7-8750H, 32 GB RAM, Fedora 32 (Linux 5.7.8-200, mitigations=off), Chrome 84.0.4147.89 (64-bit))</p>
+        <p>The benchmark was run on a Razer Blade 15 Advanced (i7-8750H, 32 GB RAM, Ubuntu 20.04 (Linux 5.4.0-42, mitigations=off), Chrome 84.0.4147.105 (64-bit))</p>
         <SelectBar  benchmarksCPU={this.state.benchmarksCPU}
                     benchmarksStartup={this.state.benchmarksStartup}
                     benchmarksMEM={this.state.benchmarksMEM}
