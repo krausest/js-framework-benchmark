9707be89878ace062943f94025b8f4ebd59b2451
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-09-23 07:54:39 -0500

    Revert etch removal
    etch has become active again:
    https://github.com/atom/etch

---
+++ frameworks/keyed/etch/src/Row.jsx
@@ -0,0 +1,44 @@
+'use strict';
+/** @jsx etch.dom */
+
+var etch = require('etch');
+
+export class Row {
+    constructor(props, children) {
+        this.props = props;
+        this.children = children;
+
+        this.onDelete = this.onDelete.bind(this);
+        this.onClick = this.onClick.bind(this);
+
+        etch.initialize(this);
+    }
+
+    update(nextProps, children) {
+        if (nextProps.data === this.props.data && nextProps.styleClass === this.props.styleClass) {
+            return Promise.resolve();
+        }
+        this.props = Object.assign({}, this.props, nextProps);
+        this.children = children;
+        return etch.update(this);
+    }
+
+    onDelete() {
+        this.props.onDelete(this.props.data.id);
+    }
+    onClick() {
+        this.props.onClick(this.props.data.id);
+    }
+
+    render() {
+        let {styleClass, onClick, onDelete, data} = this.props;
+        return (<tr className={styleClass}>
+            <td className="col-md-1">{data.id}</td>
+            <td className="col-md-4">
+                <a onClick={this.onClick}>{data.label}</a>
+            </td>
+            <td className="col-md-1"><a onClick={this.onDelete}><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></a></td>
+            <td className="col-md-6"></td>
+        </tr>);
+    }
+}
