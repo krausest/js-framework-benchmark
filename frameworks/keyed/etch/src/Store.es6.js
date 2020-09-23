7e58ab6cbe30aae99c301a1138195a4637b88293
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-09-23 08:15:33 -0500

    use const for a in swap rows

--- frameworks/keyed/etch/src/Store.es6.js
+++ frameworks/keyed/etch/src/Store.es6.js
@@ -51,7 +51,7 @@ export class Store {
     }
     swapRows() {
     	if(this.data.length > 998) {
-    		var a = this.data[1];
+    		const a = this.data[1];
     		this.data[1] = this.data[998];
     		this.data[998] = a;
     	}
