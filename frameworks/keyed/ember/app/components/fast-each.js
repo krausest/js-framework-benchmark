49e1e2e59cfa57a986f9d67369671941c1b4100d
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-10-25 12:45:58 +0300

    fix fast-each

--- frameworks/keyed/ember/app/components/fast-each.js
+++ frameworks/keyed/ember/app/components/fast-each.js
@@ -1,25 +1,24 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { scheduleOnce, cancel } from '@ember/runloop';
 export default class FastEachComponent extends Component {
   @tracked fragment = null;
   t1 = null;
   t2 = null;
   @action createRef(node) {
     this.node = node;
-    let t = Date.now();
     this.fragment = document.createDocumentFragment();
-    this.t1 = setTimeout(() => {
-      this.appendFragment();
-      this.t2 = setTimeout(() => {
-        // eslint-disable-next-line no-console
-        console.log(Date.now() - t);
-      });
-    }, 20);
+    this.t1 = scheduleOnce('afterRender', this, this.runAppend);
   }
   willDestroy() {
-    clearTimeout(this.t1);
+    cancel(this.t1);
     clearTimeout(this.t2);
+  }
+  @action runAppend() {
+    this.t2 = setTimeout(()=>{
+      this.appendFragment();
+    });
   }
   @action appendFragment() {
     this.node.appendChild(this.fragment);
