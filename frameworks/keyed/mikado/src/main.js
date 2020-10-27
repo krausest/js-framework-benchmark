f2dcad61f4b72bc093855bbf8c09fff4963096d8
Thomas Wilkerling <thomas@nextapps.de>
2020-10-27 11:50:05 +0100

    update Mikado to latest + fix issues

--- frameworks/keyed/mikado/src/main.js
+++ frameworks/keyed/mikado/src/main.js
@@ -1,32 +1,29 @@
 import Mikado from "../node_modules/mikado/src/mikado.js";
-import Array from "../node_modules/mikado/src/array.js";
-import app from "./template/app.es6.js";
-import item from "./template/item.es6.js";
-import { buildData } from "./data.js";
+import tpl_app from "./template/app.es6.js";
+import tpl_item from "./template/item.es6.js";
+import buildData from "./data.js";
 
-Mikado.once(document.getElementById("main"), app);
+Mikado.once(document.getElementById("main"), tpl_app);
 
-let selected = 0;
-const store = new Array();
-const view = new Mikado(document.getElementById("tbody"), item, {
-    "reuse": false, "store": store
-})
-.route("run", () => store.set(buildData(1000)))
-.route("runlots", () => store.set(buildData(10000)))
-.route("add", () => store.concat(buildData(1000)))
+let data = [];
+const root = document.getElementById("tbody");
+const view = new Mikado(root, tpl_item, { "reuse": false, "state": 0 })
+.route("run", () => view.render(data = buildData(1000)))
+.route("runlots", () => view.render(data = buildData(10000)))
+.route("add", () => view.append(buildData(1000)))
 .route("update", () => {
-    for(let i = 0, len = store.length; i < len; i += 10)
-        store[i].label += " !!!"
+    for(let i = 0; i < view.length; i += 10){
+        data[i].label += " !!!";
+        view.update(i, data[i]);
+    }
 })
-.route("clear", () => store.splice())
+.route("clear", () => view.clear())
 .route("swaprows", () => {
-    const tmp = store[998];
-    store[998] = store[1];
-    store[1] = tmp;
+    const tmp = data[1];
+    data[1] = data[998];
+    data[998] = tmp;
+    view.reconcile(data);
 })
-.route("remove", target => store.splice(view.index(target), 1))
-.route("select", target => {
-    store[selected]["selected"] = "";
-    store[selected = view.index(target)]["selected"] = "danger";
-})
+.route("remove", target => view.remove(target))
+.route("select", target => view.update(target, data[view.state = view.index(target)]))
 .listen("click");
