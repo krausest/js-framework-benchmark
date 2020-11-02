da0ad6220caf8d4b08696910d707dddbef0d5307
Robert Kempton <r@snow87.com>
2020-11-02 11:04:24 -0700

    get fntags from node_modules

--- frameworks/keyed/fntags/build.js
+++ frameworks/keyed/fntags/build.js
@@ -14,5 +14,5 @@ const minify = ( file ) => {
         return code
     }
 }
-fs.writeFileSync('dist/fntags.min.js', minify('src/fntags.js'))
+fs.writeFileSync('dist/fntags.min.js', minify('node_modules/fntags/src/fntags.js'))
 fs.writeFileSync( 'dist/Main.js', minify( 'src/Main.js' ) )
