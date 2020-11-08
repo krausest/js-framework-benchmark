6d8f31e989bdf32f89718d8f820b4fad77b7f038
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-11-08 18:53:15 +0300

    simplify update logic

--- frameworks/keyed/ember/app/services/state.js
+++ frameworks/keyed/ember/app/services/state.js
@@ -3,17 +3,16 @@ import Service from '@ember/service';
 export default class StateService extends Service {
   helpers = new Map();
   prevId = null;
+  updateHelper(id, isSelected) {
+    const instance = this.helpers.get(id)
+    if (instance) {
+      instance.isSelected = isSelected;
+      instance.recompute();
+    }
+  }
   updateSelection(id) {
-    const oldInstance = this.helpers.get(this.prevId)
-    if (oldInstance) {
-      oldInstance.isSelected = false;
-      oldInstance.recompute();
-    }
+    this.updateHelper(this.prevId, false);
     this.prevId = id;
-    const newInstance = this.helpers.get(this.prevId)
-    if (newInstance) {
-      newInstance.isSelected = true;
-      newInstance.recompute();
-    }
+    this.updateHelper(this.prevId, true);
   }
 }
