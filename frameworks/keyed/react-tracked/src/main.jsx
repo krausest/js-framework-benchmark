ae7bbff725472c8b3d40cf394f4250d25afbcd92
daishi <daishi@axlight.com>
2020-12-27 16:41:42 +0900

    update react-tracked v1.6.0

--- frameworks/keyed/react-tracked/src/main.jsx
+++ frameworks/keyed/react-tracked/src/main.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useReducer } from "react";
+import React, { useReducer } from "react";
 import ReactDOM from "react-dom";
 import { createContainer, memo } from "react-tracked";
 
@@ -67,13 +67,11 @@ const GlyphIcon = <span className="glyphicon glyphicon-remove" aria-hidden="true
 
 const Row = memo(({ item, isSelected }) => {
   const dispatch = useDispatch();
-  const select = useCallback(() => { dispatch({ type: "SELECT", id: item.id }); }, [item.id]);
-  const remove = useCallback(() => { dispatch({ type: "REMOVE", id: item.id }); }, [item.id]);
   return (
     <tr className={isSelected ? "danger" : ""}>
       <td className="col-md-1">{item.id}</td>
-      <td className="col-md-4"><a onClick={select}>{item.label}</a></td>
-      <td className="col-md-1"><a onClick={remove}>{GlyphIcon}</a></td>
+      <td className="col-md-4"><a onClick={() => dispatch({ type: "SELECT", id: item.id })}>{item.label}</a></td>
+      <td className="col-md-1"><a onClick={() => dispatch({ type: "REMOVE", id: item.id })}>{GlyphIcon}</a></td>
       <td className="col-md-6"></td>
     </tr>
   )
@@ -92,24 +90,18 @@ const Button = memo(({ id, title, cb }) => (
 
 const Main = () => {
   const dispatch = useDispatch();
-  const run = useCallback(() => { dispatch({ type: "RUN" }); }, []);
-  const runLots = useCallback(() => { dispatch({ type: "RUN_LOTS" }); }, []);
-  const add = useCallback(() => { dispatch({ type: "ADD" }); }, []);
-  const update = useCallback(() => { dispatch({ type: "UPDATE" }); }, []);
-  const clear = useCallback(() => { dispatch({ type: "CLEAR" }); }, []);
-  const swapRows = useCallback(() => { dispatch({ type: "SWAP_ROWS" }); }, []);
   return (
     <div className="container">
       <div className="jumbotron">
         <div className="row">
           <div className="col-md-6"><h1>React Tracked</h1></div>
           <div className="col-md-6"><div className="row">
-            <Button id="run" title="Create 1,000 rows" cb={run} />
-            <Button id="runlots" title="Create 10,000 rows" cb={runLots} />
-            <Button id="add" title="Append 1,000 rows" cb={add} />
-            <Button id="update" title="Update every 10th row" cb={update} />
-            <Button id="clear" title="Clear" cb={clear} />
-            <Button id="swaprows" title="Swap Rows" cb={swapRows} />
+            <Button id="run" title="Create 1,000 rows" cb={() => dispatch({ type: "RUN" })} />
+            <Button id="runlots" title="Create 10,000 rows" cb={() => dispatch({ type: "RUN_LOTS" })} />
+            <Button id="add" title="Append 1,000 rows" cb={() => dispatch({ type: "ADD" })} />
+            <Button id="update" title="Update every 10th row" cb={() => dispatch({ type: "UPDATE" })} />
+            <Button id="clear" title="Clear" cb={() => dispatch({ type: "CLEAR" })} />
+            <Button id="swaprows" title="Swap Rows" cb={() => dispatch({ type: "SWAP_ROWS" })} />
           </div></div>
         </div>
       </div>
