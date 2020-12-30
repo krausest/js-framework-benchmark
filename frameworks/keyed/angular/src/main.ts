1b967e27421462e526852ab2d23cc7e4ceb7fb96
Miroslav Jonas <missing.manual@gmail.com>
2020-12-30 15:04:41 +0100

    feat(angular): rename projects for better report readability

--- frameworks/keyed/angular/src/main.ts
+++ frameworks/keyed/angular/src/main.ts
@@ -1,6 +1,12 @@
-import { platformBrowser } from '@angular/platform-browser';
 import { enableProdMode } from '@angular/core';
-import { AppModuleNgFactory } from './app.ngfactory';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-enableProdMode();
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory)
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+// if (environment.production) {
+  enableProdMode();
+// }
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
