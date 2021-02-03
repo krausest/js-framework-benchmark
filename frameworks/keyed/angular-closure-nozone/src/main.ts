e4ca071046567102cf0917d4824ce02047e7e444
Miroslav Jonas <missing.manual@gmail.com>
2021-02-03 10:25:19 +0100

    feat(angular-closure-nozone): cleanup main

--- frameworks/keyed/angular-closure-nozone/src/main.ts
+++ frameworks/keyed/angular-closure-nozone/src/main.ts
@@ -1,15 +1,7 @@
 import { platformBrowser } from '@angular/platform-browser';
-import { enableProdMode, ApplicationRef } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 import { AppModule } from './app';
 
-window['Zone'] = {
-  get current() { return this },
-  assertZonePatched() { },
-  fork() { return this },
-  get() { return true },
-  run(fn: Function) { return fn() },
-  runGuarded(fn: Function) { return fn() },
-}
-
 enableProdMode();
-platformBrowser().bootstrapModule(AppModule, { ngZone: 'noop' });
+platformBrowser().bootstrapModule(AppModule, { ngZone: 'noop' })
+  .catch(err => console.error(err));;
