0acab9c61db1fc671265a668dd0157a714b24abc
Stefan Krause <stefan.krause@gmx.at>
2021-02-06 21:41:28 +0100

    retire angular closure version

--- frameworks/keyed/angular-closure/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { enableProdMode } from '@angular/core';
-import { AppModule } from './app';
-
-enableProdMode();
-platformBrowser().bootstrapModule(AppModule);
