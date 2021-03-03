7c09d1d0c3817614c41d1d7958acdb921ca85ff1
Mathis Møller <mmm@cryosinternational.com>
2021-03-03 10:54:03 +0100

    Add atoms in atom benchmark

--- frameworks/keyed/jotai-atoms-in-atom/src/main.tsx
+++ frameworks/keyed/jotai-atoms-in-atom/src/main.tsx
@@ -1,47 +1,57 @@
 import { memo, FC } from "react";
 import ReactDOM from "react-dom";
-import { atom, useAtom } from "jotai";
+import { atom, useAtom, PrimitiveAtom } from "jotai";
 import { useUpdateAtom, useAtomValue } from "jotai/utils";
 
 import { buildData, Data } from "./utils";
 
-const stateAtom = atom<{ data: Data[]; selected: number }>({
+function buildAtomData(amount: number) {
+  return buildData(amount).map((data) => atom(data));
+}
+
+const stateAtom = atom<{ data: PrimitiveAtom<Data>[]; selected: number }>({
   data: [],
   selected: 0,
 });
 
+const selectedIdAtom = atom((get) => get(stateAtom).selected);
+
 const createRowsAtom = atom(null, (_, set, amount: number) =>
-  set(stateAtom, { data: buildData(amount), selected: 0 })
+  set(stateAtom, { data: buildAtomData(amount), selected: 0 })
 );
 
 const appendRowsAtom = atom(null, (_, set) =>
   set(stateAtom, (state) => ({
-    data: state.data.concat(buildData(1000)),
+    data: state.data.concat(buildAtomData(1000)),
     selected: 0,
   }))
 );
 
-const updateRowsAtom = atom(null, (_, set) =>
+const updateRowsAtom = atom(null, (get, set) =>
   set(stateAtom, ({ data, selected }) => {
     const newData = data.slice(0);
     for (let i = 0; i < newData.length; i += 10) {
       const r = newData[i];
-      newData[i] = { id: r.id, label: r.label + " !!!" };
+      newData[i] = atom({ id: get(r).id, label: get(r).label + " !!!" });
     }
     return { data: newData, selected };
   })
 );
 
-const removeRowAtom = atom(null, (_, set, id: number) =>
+const removeRowAtom = atom(null, (_, set, atom: PrimitiveAtom<Data>) =>
   set(stateAtom, ({ data, selected }) => {
-    const idx = data.findIndex((d) => d.id === id);
+    const idx = data.findIndex((d) => d === atom);
     return { data: [...data.slice(0, idx), ...data.slice(idx + 1)], selected };
   })
 );
 
-const selectRowAtom = atom(null, (_, set, selected: number) =>
-  set(stateAtom, (state) => ({ data: state.data, selected }))
-);
+const selectRowAtom = atom(null, (get, set, atom: PrimitiveAtom<Data>) => {
+  const rowAtoms = get(stateAtom);
+  const rowAtom = rowAtoms.data.find((rowAtom) => rowAtom === atom);
+  if (rowAtom) {
+    set(stateAtom, (prev) => ({ data: prev.data, selected: get(rowAtom).id }));
+  }
+});
 
 const clearStateAtom = atom(null, (_, set) =>
   set(stateAtom, () => ({
@@ -67,22 +77,23 @@ const GlyphIcon = (
 );
 
 interface RowProps {
-  id: number;
-  label: string;
-  isSelected: boolean;
+  atom: PrimitiveAtom<Data>;
 }
 
-const Row = memo<RowProps>(({ id, label, isSelected }) => {
-  const selectRow = useUpdateAtom(selectRowAtom);
-  const removeRow = useUpdateAtom(removeRowAtom);
+const Row = memo<RowProps>(({ atom }) => {
+  const rowAtom = useAtomValue(atom);
+  const [, selectRow] = useAtom(selectRowAtom);
+  const [, removeRow] = useAtom(removeRowAtom);
+  const selectedRow = useAtomValue(selectedIdAtom);
+  const isSelected = rowAtom.id === selectedRow;
   return (
     <tr className={isSelected ? "danger" : ""}>
-      <td className="col-md-1">{id}</td>
+      <td className="col-md-1">{rowAtom.id}</td>
       <td className="col-md-4">
-        <a onClick={() => selectRow(id)}>{label}</a>
+        <a onClick={() => selectRow(atom)}>{rowAtom.label}</a>
       </td>
       <td className="col-md-1">
-        <a onClick={() => removeRow(id)}>{GlyphIcon}</a>
+        <a onClick={() => removeRow(atom)}>{GlyphIcon}</a>
       </td>
       <td className="col-md-6"></td>
     </tr>
@@ -90,16 +101,11 @@ const Row = memo<RowProps>(({ id, label, isSelected }) => {
 });
 
 const RowList = memo(() => {
-  const { data, selected } = useAtomValue(stateAtom);
+  const { data } = useAtomValue(stateAtom);
   return (
     <>
-      {data.map((item) => (
-        <Row
-          key={item.id}
-          id={item.id}
-          label={item.label}
-          isSelected={selected === item.id}
-        />
+      {data.map((atom) => (
+        <Row key={String(atom)} atom={atom} />
       ))}
     </>
   );
