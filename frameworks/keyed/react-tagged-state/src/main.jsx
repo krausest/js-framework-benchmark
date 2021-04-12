985aa476f13d33612795983c446b1ff3064ba8af
Олег Гришечкин <oleggrishechkin@gmail.com>
2021-04-12 22:17:37 +0300

    Update react-tagged-state

--- frameworks/keyed/react-tagged-state/src/main.jsx
+++ frameworks/keyed/react-tagged-state/src/main.jsx
@@ -1,6 +1,6 @@
-import React  from "react";
-import ReactDOM from "react-dom";
-import {createTagged, useSelector, useTagged} from "react-tagged-state";
+import { memo }  from "react";
+import { render } from "react-dom";
+import { createTagged , useTagged } from "react-tagged-state";
 
 const A = ["pretty", "large", "big", "small", "tall", "short", "long", "handsome", "plain", "quaint", "clean",
   "elegant", "easy", "angry", "crazy", "helpful", "mushy", "odd", "unsightly", "adorable", "important", "inexpensive",
@@ -28,112 +28,93 @@ const buildData = (count) => {
 }
 
 const dataState = createTagged([]);
-const selectionState = createTagged(0);
 
-const run = () => {
-  dataState(buildData(1000));
-  selectionState(0);
-};
-const runLots = () => {
-  dataState(buildData(10000));
-  selectionState(0);
-};
-const add = () => {
-  dataState((data) => [...data, ...buildData(1000)]);
-};
-const update = () => {
-  dataState((data) => {
-    const newData = [...data];
-    let index = 0;
-    let length = newData.length;
-    let temp;
+const selectedState = createTagged(0);
 
-    for (; index < length; index += 10) {
-      temp = newData[index];
+const update = (data) => {
+  const newData = data.slice();
+  let index = 0;
+  let length = newData.length;
+  let temp;
 
-      newData[index] = { id: temp.id, label: temp.label + " !!!" };
-    }
+  for (; index < length; index += 10) {
+    temp = newData[index];
 
-    return newData;
-  });
-};
-const remove = (item) => {
-  dataState((data) => {
-    const newData = [...data];
+    newData[index] = { id: temp.id, label: temp.label + " !!!" };
+  }
 
-    newData.splice(data.indexOf(item), 1);
-
-    return newData;
-  });
-};
-const select = (id) => {
-  selectionState(id);
-};
-const clear = () => {
-  dataState([]);
-  selectionState(0);
-};
-const swapRows = () => {
-  dataState((data) => {
-    const newData = [...data];
-    const tmp = newData[1];
-
-    newData[1] = newData[998];
-    newData[998] = tmp;
-
-    return newData;
-  });
+  return newData;
 };
 
+const remove = (item) => (data) => {
+  const newData = data.slice();
 
-const GlyphIcon = <span className="glyphicon glyphicon-remove" aria-hidden="true"/>;
+  newData.splice(data.indexOf(item), 1);
 
-const Row = React.memo(({ data }) => {
-  const isSelected = useSelector(() => selectionState() === data.id);
+  return newData;
+};
 
-  return (
-      <tr className={isSelected ? "danger" : ""}>
-        <td className="col-md-1">{data.id}</td>
-        <td className="col-md-4"><a onClick={() => select(data.id)}>{data.label}</a></td>
-        <td className="col-md-1"><a onClick={() => remove(data)}>{GlyphIcon}</a></td>
-        <td className="col-md-6"/>
-      </tr>
-  )
-});
+const swapRows = (data) => {
+  const newData = data.slice();
+  const tmp = newData[1];
 
-const RowList = React.memo(() => {
-  useTagged();
+  newData[1] = newData[998];
+  newData[998] = tmp;
 
-  return dataState().map((data) => <Row key={data.id} data={data} />);
-});
+  return newData;
+};
 
-const Button = React.memo(({ id, title, cb }) => (
-    <div className="col-sm-6 smallpad">
-      <button type="button" className="btn btn-primary btn-block" id={id} onClick={cb}>{title}</button>
-    </div>
+const Row = memo(({ item, isSelected }) => (
+    <tr className={isSelected ? "danger" : ""}>
+      <td className="col-md-1">{item.id}</td>
+      <td className="col-md-4">
+        <a onClick={() => selectedState(item.id)}>{item.label}</a>
+      </td>
+      <td className="col-md-1">
+        <a onClick={() => dataState(remove(item))}><span className="glyphicon glyphicon-remove" aria-hidden="true"/></a>
+      </td>
+      <td className="col-md-6"/>
+    </tr>
 ));
 
-const Main = () => (
-    <div className="container">
-      <div className="jumbotron">
-        <div className="row">
-          <div className="col-md-6"><h1>React Tagged State </h1></div>
-          <div className="col-md-6"><div className="row">
-            <Button id="run" title="Create 1,000 rows" cb={run} />
-            <Button id="runlots" title="Create 10,000 rows" cb={runLots} />
-            <Button id="add" title="Append 1,000 rows" cb={add} />
-            <Button id="update" title="Update every 10th row" cb={update} />
-            <Button id="clear" title="Clear" cb={clear} />
-            <Button id="swaprows" title="Swap Rows" cb={swapRows} />
-          </div></div>
-        </div>
-      </div>
-      <table className="table table-hover table-striped test-data"><tbody><RowList /></tbody></table>
-      <span className="preloadicon glyphicon glyphicon-remove" aria-hidden="true"/>
-    </div>
+const RowList = () => {
+  const data = useTagged(dataState);
+  const selected = useTagged(selectedState);
+
+  return data.map((item) => <Row key={item.id} item={item} isSelected={item.id === selected} />);
+};
+
+const Button = ({ id, title, cb }) => (
+  <div className="col-sm-6 smallpad">
+    <button type="button" className="btn btn-primary btn-block" id={id} onClick={cb}>{title}</button>
+  </div>
 );
 
-ReactDOM.render(
-    <Main />,
-    document.getElementById("main")
+const Main = () => (
+  <div className="container">
+    <div className="jumbotron">
+      <div className="row">
+        <div className="col-md-6"><h1>React Tagged State </h1></div>
+        <div className="col-md-6"><div className="row">
+          <Button id="run" title="Create 1,000 rows" cb={() => dataState(buildData(1000))} />
+          <Button id="runlots" title="Create 10,000 rows" cb={() => dataState(buildData(10000))} />
+          <Button id="add" title="Append 1,000 rows" cb={() => dataState((data) => data.concat(buildData(1000)))} />
+          <Button id="update" title="Update every 10th row" cb={() => dataState(update)} />
+          <Button id="clear" title="Clear" cb={() => dataState([])} />
+          <Button id="swaprows" title="Swap Rows" cb={() => dataState(swapRows)} />
+        </div></div>
+      </div>
+    </div>
+    <table className="table table-hover table-striped test-data">
+      <tbody>
+        <RowList />
+      </tbody>
+    </table>
+    <span className="preloadicon glyphicon glyphicon-remove" aria-hidden="true"/>
+  </div>
+);
+
+render(
+  <Main />,
+  document.getElementById("main")
 );
