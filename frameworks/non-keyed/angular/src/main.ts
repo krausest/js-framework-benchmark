7750ec99b6e516fa4304ee09fe6f086e705687b5
Stefan Krause <Stefan.Krause@gmx.at>
2021-05-24 21:04:25 +0200

    update angular, remove non-keyed angular

--- frameworks/non-keyed/angular/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { enableProdMode, ApplicationRef } from '@angular/core';
-import { AppModuleNgFactory } from './app.ngfactory';
-
-enableProdMode();
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory)
