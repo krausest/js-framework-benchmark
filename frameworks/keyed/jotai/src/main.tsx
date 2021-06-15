99c5766db849ac90f2604e98365b2b09ef6aad41
daishi <daishi@axlight.com>
2021-06-15 19:33:05 +0900

    split utils

--- frameworks/keyed/jotai/src/main.tsx
+++ frameworks/keyed/jotai/src/main.tsx
@@ -3,37 +3,7 @@ import ReactDOM from "react-dom";
 import { atom, useAtom, PrimitiveAtom } from "jotai";
 import { useUpdateAtom } from "jotai/utils";
 
-function random(max: number) {
-  return Math.round(Math.random() * 1000) % max;
-}
-
-const A = ["pretty", "large", "big", "small", "tall", "short", "long", "handsome", "plain", "quaint", "clean", "elegant", "easy", "angry", "crazy", "helpful", "mushy", "odd", "unsightly", "adorable", "important", "inexpensive", "cheap", "expensive", "fancy"];
-const C = ["red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black", "orange"];
-const N = ["table", "chair", "house", "bbq", "desk", "car", "pony", "cookie", "sandwich", "burger", "pizza", "mouse", "keyboard"];
-
-let nextId = 1;
-
-interface Data {
-  id: number;
-  label: string;
-}
-
-function buildNextItem(): Data {
-  return {
-    id: nextId++,
-    label: `${A[random(A.length)]} ${C[random(C.length)]} ${
-      N[random(N.length)]
-    }`,
-  };
-}
-
-function buildDataAtoms(count: number): PrimitiveAtom<Data>[] {
-  const data = new Array(count);
-  for (let i = 0; i < count; i++) {
-    data[i] = atom(buildNextItem());
-  }
-  return data;
-}
+import { Data, buildNextItem, buildDataAtoms } from "./utils";
 
 const dataAtom = atom<PrimitiveAtom<Data>[]>([]);
 const selectedAtom = atom<PrimitiveAtom<Data> | null>(null);
