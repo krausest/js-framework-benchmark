ab7d867a3bc193da49eec60ea069cd263808b6af
webreflection <andrea.giammarchi@gmail.com>
2021-06-18 12:05:56 +0200

    Update uhtml to v2.7.5

--- frameworks/keyed/uhtml/src/index.js
+++ frameworks/keyed/uhtml/src/index.js
@@ -2,18 +2,14 @@ import {State} from 'js-framework-benchmark-utils';
 import {html, render} from 'uhtml';
 
 import Jumbotron from './jumbotron.js';
-import Table from './table.js';
+import Table from './table-delegate.js';
 
-const state = State(update);
-const main = document.getElementById('container');
+const state = State(Table);
 
-update(state);
-
-function update(state) {
-  render(main, html`
+render(document.getElementById('container'), html`
   <div class="container">
     ${Jumbotron(state)}
     ${Table(state)}
     <span class="preloadicon glyphicon glyphicon-remove" aria-hidden="true" />
-  </div>`);
-}
+  </div>
+`);
