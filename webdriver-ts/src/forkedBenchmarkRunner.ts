6dc414039ac3b9d41427881a28e9e79ccfdb35fb
Stefan Krause <stefan.krause@gmx.at>
2021-09-25 14:08:43 +0200

    remove screenshots from trace, minimize trace categories

--- webdriver-ts/src/forkedBenchmarkRunner.ts
+++ webdriver-ts/src/forkedBenchmarkRunner.ts
@@ -222,7 +222,12 @@ async function runCPUBenchmark(framework: FrameworkData, benchmark: Benchmark, b
             await initBenchmark(page, benchmark, framework);
 
    
-            await page.tracing.start({path: fileNameTrace(framework, benchmark, i), screenshots: true});
+            await page.tracing.start({path: fileNameTrace(framework, benchmark, i), 
+                screenshots: false,
+                categories:[ "devtools.timeline","blink.user_timing"]
+                },
+                
+                );
             console.log("runBenchmark");
             await runBenchmark(page, benchmark, framework);
             await wait(40);
