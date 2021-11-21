c8f5b05e90cd78ced2bdc19ed60f994d80be9f89
Stefan Krause <Stefan.Krause@gmx.at>
2021-11-21 19:51:17 +0100

    update angularjs

--- webdriver-ts-results/src/App.tsx
+++ webdriver-ts-results/src/App.tsx
@@ -6,7 +6,7 @@ import { SelectionBar } from './selection/SelectionBar';
 
 const App = () => {
   // eslint-disable-next-line no-constant-condition
-  const disclaimer = (true) ? (<div>
+  const disclaimer = (false) ? (<div>
         <h2>Results for js web frameworks benchmark - official run</h2>
         <p>A description of the benchmark and the source code and can be found in the github <a href="https://github.com/krausest/js-framework-benchmark">repository</a>.</p>
       </div>) :
