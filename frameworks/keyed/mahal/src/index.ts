d5c16ac115649918dea633341ffc61c5513fc0b8
Ujjwal Gupta <ujjwalkumargupta4u@gmail.com>
2022-06-25 16:45:30 +0530

    rm unused code

--- frameworks/keyed/mahal/src/index.ts
+++ frameworks/keyed/mahal/src/index.ts
@@ -1,12 +1,6 @@
 import { Mahal } from "mahal";
 import App from "@/app.mahal";
-import { registerGlobalFormatter } from "@/formatters";
-import config from "~/config/env";
 
 
 const app = new Mahal(App, '#main');
-// register global formatter
-registerGlobalFormatter(app);
-// set config to be available globally
-app.global.config = config;
 app.create();
