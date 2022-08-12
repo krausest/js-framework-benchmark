41d0af3e28b37db1cde5633c21042e063e8905bc
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-08-12 10:40:12 -0400

    Too much shadowdom and the test analyzer can't detect anything

--- frameworks/keyed/lit-starbeam/src/table.ts
+++ frameworks/keyed/lit-starbeam/src/table.ts
@@ -6,24 +6,40 @@ import { cached } from "@starbeam/js";
 import { starbeam } from "./integration";
 import { Item, Table } from "./types";
 
+@customElement('label-element')
+export class LabelElement extends LitElement {
+  @property() table: Table;
+  @property() item: Item;
+
+  @starbeam
+  render() {
+    let select = () => this.table.select(this.item.id);
+    return html`<a @click=${select}>${this.item.label}</a>`;
+  }
+}
+
 @customElement('table-element')
 export class TableElement extends LitElement {
   @property() table: Table;
 
+
+
   @cached
   get rows() {
+    let selected = this.table.selected;
+
     return FormulaList(this.table.data, {
       key: item => `${item.id}-${this.table.selected}`,
-      value: ({ id, label }) => {
-        let isSelected = id === this.table.selected;
-        let select = () => this.table.select(id);
+      value: (item) => {
+        let { id } = item;
+        let isSelected = id === selected;
         let remove = () => this.table.remove(id);
 
         return html`
           <tr id=${id} class=${isSelected ? 'danger' : ''}>
             <td class="col-md-1">${id}</td>
             <td class="col-md-4">
-              <a @click=${select}>${label}</a>
+              <label-element .table=${this.table} .item=${item}></label-element>
             </td>
             <td class="col-md-1">
               <a @click=${remove}>
@@ -39,8 +55,6 @@ export class TableElement extends LitElement {
 
   @starbeam
   render() {
-    let selected = this.table.selected;
-
     return html`
       <table class="table table-hover table-striped test-data">
         <tbody>${this.rows.current}</tbody>
