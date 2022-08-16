a43340dc823727494c544c6b6ef93728d4fb2e46
Ryan Carniato <ryansolid@gmail.com>
2022-08-16 14:56:03 -0700

    initial qwik implementation

---
+++ frameworks/keyed/qwik/rollup.config.js
@@ -0,0 +1,27 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { qwikRollup } from '@builder.io/qwik/optimizer';
+import { terser } from 'rollup-plugin-terser';
+import { resolve } from 'path';
+
+export default async function () {
+  return {
+    input: [
+      'src/main.tsx'
+    ],
+    preserveEntrySignatures: true,
+    plugins: [
+      nodeResolve({ extensions: [".tsx", ".ts", ".jsx", ".js"] }),
+      qwikRollup({
+        srcDir: resolve('./src'),
+        entryStrategy: {type: 'single' }
+      }),
+      terser()
+    ],
+    output:
+      {
+        chunkFileNames: 'q-[hash].js',
+        dir: 'dist',
+        format: 'es',
+      },
+  };
+}
