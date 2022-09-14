3a60087d24a91e2662979796273df2ef863a1858
Jason Antwi-Appah <jason@allotrope.dev>
2022-09-14 14:05:57 -0500

    chore: rm keyed million benches

--- frameworks/keyed/million-react/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import { million } from 'million/vite-plugin-million';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [million({ react: true })],
-});
