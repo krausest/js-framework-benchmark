ae30e96dbae46c75d3522482ab97bbe9f1037ab6
Rahim Alwer <rahim.alwer@gmail.com>
2022-12-27 14:37:42 +1100

    add maverick to keyed bench

---
+++ frameworks/keyed/maverick/build.js
@@ -0,0 +1,20 @@
+const { esbuild: maverick } = require("@maverick-js/compiler");
+
+const dev = process.argv.includes("--dev");
+
+require("esbuild").build({
+  entryPoints: ["src/main.tsx"],
+  outfile: "dist/main.js",
+  watch: dev,
+  bundle: true,
+  minify: !dev,
+  platform: "browser",
+  target: "es2020",
+  conditions: [dev ? "development" : "production", "default"],
+  plugins: [
+    maverick({
+      include: ["src/**/*.tsx"],
+      delegateEvents: true,
+    }),
+  ],
+});
