521bc0f531be5cb51a1b9949c69fb0d4acee9c66
Vasilis Themelis <vdthemelis@gmail.com>
2023-01-25 19:50:17 +0000

    Fix incr_dom build
    * Upgrade the versions of various dependencies
    * Force compiling to OCaml bytecode so that js_of_ocaml can transpile
      that to JS

--- broken-frameworks/non-keyed/incr_dom/copy.sh
+++ broken-frameworks/non-keyed/incr_dom/copy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -x
 set -x
 
-RELEASE_PATH="$(pwd)/dist"
+RELEASE_PATH="$(pwd)/dist/"
 mkdir -p $RELEASE_PATH
 
-built=$(find | grep .bc.js | grep src)
+built=$(find . | grep .bc.js | grep src)
 rm -f $RELEASE_PATH/*.bc.js
 cp $built $RELEASE_PATH
 
-built=$(find | grep .bc.map | grep src)
+built=$(find . | grep .bc.map | grep src)
 if [ ! -z "$built" ]; then
   rm -f $RELEASE_PATH/*.bc.map
   cp $built $RELEASE_PATH
--- broken-frameworks/non-keyed/incr_dom/src/components/Util.re
+++ broken-frameworks/non-keyed/incr_dom/src/components/Util.re
@@ -1,4 +1,4 @@
-open! Core_kernel;
+open! Core;
 
 let adjectives = [|
   "pretty",
