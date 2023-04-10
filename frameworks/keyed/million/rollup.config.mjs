6353829d4c8c5104283aaf09ebe6e0ab38dcf917
Aiden Bai <aiden.bai05@gmail.com>
2023-04-10 12:17:40 -0700

    refactor: rollup config

--- frameworks/keyed/million/rollup.config.mjs
+++ frameworks/keyed/million/rollup.config.mjs
@@ -1,42 +1,38 @@
 import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
-import { URL } from 'url';
 import million from 'million/compiler';
 
-const plugins = [
-  resolve({
-    preferBuiltins: false,
-    extensions: ['.js', '.jsx'],
-  }),
-  million.rollup(),
-  terser({
-    parse: {
-      ecma: 8,
-    },
-    compress: {
-      ecma: 5,
-      inline: true,
-      if_return: false,
-      reduce_funcs: false,
-      passes: 5,
-      comparisons: false,
-    },
-    output: {
-      ecma: 5,
-      comments: false,
-    },
-    toplevel: true,
-    module: true,
-  }),
-];
+const TERSER_OPTIONS = {
+  compress: {
+    ecma: 5,
+    inline: true,
+    if_return: false,
+    reduce_funcs: false,
+    passes: 5,
+    comparisons: false,
+  },
+  toplevel: true,
+  mangle: true,
+  module: true,
+};
 
 export default {
-  input: new URL('src/main.jsx', import.meta.url).pathname,
+  input: 'src/main.jsx',
   output: {
     name: 'million',
     format: 'iife',
-    file: new URL('dist/main.js', import.meta.url).pathname,
+    file: 'dist/main.js',
     sourcemap: false,
   },
-  plugins,
+  watch: {
+    clearScreen: false,
+  },
+  plugins: [
+    resolve({
+      preferBuiltins: false,
+      extensions: ['.js', '.jsx'],
+    }),
+    million.rollup(),
+    terser(TERSER_OPTIONS),
+  ],
 };
