517548cd445711d24403870621f1e005ec118719
Enea Jahollari <jahollarienea14@gmail.com>
2023-09-02 21:35:17 +0300

    fix: simplify angular, and use esbuild builder

--- frameworks/keyed/angular-nozone/src/app/app.config.ts
+++
@@ -1,8 +0,0 @@
-import { ApplicationConfig, NgZone, ɵNoopNgZone } from '@angular/core';
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    // https://github.com/angular/angular/issues/47538
-    { provide: NgZone, useClass: ɵNoopNgZone }
-  ]
-};
