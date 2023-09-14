b50b3aa080f9aff57a473b5d72824a0dd645f8f8
Stefan Krause <stefan.krause@gmx.at>
2023-09-14 21:23:27 +0200

    results for chrome 117

--- webdriver-ts-results/src/App.tsx
+++ webdriver-ts-results/src/App.tsx
@@ -15,7 +15,7 @@ const App = (): JSX.Element => {
     return (
       <div>
         {disclaimer}
-        <p>The benchmark was run on a MacBook Pro 14 (32 GB RAM, 8/14 Cores, OSX 13.5 (c)), Chrome 116.0.5845.82 (arm64))
+        <p>The benchmark was run on a MacBook Pro 14 (32 GB RAM, 8/14 Cores, OSX 13.5.2), Chrome 117.0.5938.62 (arm64)
             using the puppeteer benchmark driver with reduced tracing.
         </p>
          <SelectionBar showDurationSelection={false}/>
