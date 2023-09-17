b4ceefa9ec485ca9eee10ff373be2a61fa21d08d
Stefan Krause <stefan.krause@gmx.at>
2023-09-17 22:05:50 +0200

    workaround for issue 1368 (no one uses a ssh tunnel but me)

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 const server = buildServer();
 
 try {
-  await server.listen({ port: PORT, host: '0.0.0.0' });
+  await server.listen({ port: PORT}); //, host: '0.0.0.0' });
   console.log(`Server running on port ${PORT}`);
 } catch (error) {
   server.log.error(error);
