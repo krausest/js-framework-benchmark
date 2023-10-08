d028b05719e336953c1a5bba6cd7b5f9600a5f3b
Daniil Zhuravlev <nakrovati@outlook.com>
2023-10-08 15:59:23 +0400

    refactor(web-ts-results): add React.StrictMode

--- webdriver-ts-results/src/index.tsx
+++ webdriver-ts-results/src/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import App from "./App";
 import { createRoot } from "react-dom/client";
 
-window.onload = () => {
-  const root = createRoot(document.getElementById("root")!);
-
-  root.render(<App />);
-};
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
