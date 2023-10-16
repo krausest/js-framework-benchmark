d496473829f61bc53f55bceb25ad6cf077c76688
Birk Skyum <birk.skyum@pm.me>
2023-10-16 18:10:12 +0200

    preact-hooks

---
+++ frameworks/keyed/preact-hooks/rollup.config.js
@@ -0,0 +1,30 @@
+import resolve from "@rollup/plugin-node-resolve";
+import { babel } from "@rollup/plugin-babel";
+import terser from "@rollup/plugin-terser";
+
+const plugins = [
+  babel({
+    babelHelpers: "bundled",
+    exclude: "node_modules/**",
+    presets: ["babel-preset-preact"],
+    plugins: [
+      ["@babel/plugin-transform-react-jsx", {
+        "pragma": "h"
+      }]
+    ]
+  }),
+  resolve({ extensions: [".js", ".jsx"] }),
+];
+
+if (process.env.production) {
+  plugins.push(terser());
+}
+
+export default {
+  input: "src/main.jsx",
+  output: {
+    file: "dist/main.js",
+    format: "iife",
+  },
+  plugins,
+};
