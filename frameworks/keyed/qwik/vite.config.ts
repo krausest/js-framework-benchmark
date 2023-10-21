0404bfadd01a92dbb3482102bfe90d63a63f0393
Wout Mertens <Wout.Mertens@gmail.com>
2023-10-21 12:06:46 +0200

    qwik: update and use SSG

---
+++ frameworks/keyed/qwik/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikCity } from "@builder.io/qwik-city/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig(() => {
+  return {
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    preview: {
+      headers: {
+        "Cache-Control": "public, max-age=600",
+      },
+    },
+    resolve: {
+      alias: {
+        // The benchmark CSS
+        "/css": "../../../css",
+      },
+    },
+    server: {
+      fs: {
+        allow: ["../../.."],
+      },
+    },
+  };
+});
