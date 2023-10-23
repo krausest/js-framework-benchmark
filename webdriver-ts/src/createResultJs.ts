bc7ba61150f82f214384dd854cedc28cc3d4cc57
Daniil Zhuravlev <nakrovati@outlook.com>
2023-10-23 23:28:08 +0400

    fix(webdriver): name all files in CamelCase style

---
+++ webdriver-ts/src/createResultJs.ts
@@ -0,0 +1,158 @@
+import * as fs from "node:fs";
+import yargs from "yargs";
+import { BenchmarkInfo, benchmarkInfos, BenchmarkType, fileName, slowDownFactor } from "./benchmarksCommon.js";
+import { subbenchmarks } from "./benchmarksLighthouse.js";
+import { BenchmarkOptions, config, initializeFrameworks, JsonResult } from "./common.js";
+
+let args: any = yargs(process.argv)
+  .usage(
+    "$0 [--framework Framework1 Framework2 ...] [--benchmark Benchmark1 Benchmark2 ...] [--chromeBinary path] \n or: $0 [directory1] [directory2] .. [directory3]"
+  )
+  .help("help")
+  .string("runner")
+  .default("runner", "puppeteer")
+  .string("browser").argv;
+
+console.log("args", args);
+
+let benchmarkOptions: BenchmarkOptions = {
+  port: 8080,
+  host: "localhost",
+  browser: args.browser,
+  remoteDebuggingPort: 9999,
+  chromePort: 9998,
+  headless: args.headless,
+  chromeBinaryPath: args.chromeBinary,
+  numIterationsForCPUBenchmarks:
+    config.NUM_ITERATIONS_FOR_BENCHMARK_CPU + config.NUM_ITERATIONS_FOR_BENCHMARK_CPU_DROP_SLOWEST_COUNT,
+  numIterationsForMemBenchmarks: config.NUM_ITERATIONS_FOR_BENCHMARK_MEM,
+  numIterationsForStartupBenchmark: config.NUM_ITERATIONS_FOR_BENCHMARK_STARTUP,
+  batchSize: 1,
+  resultsDirectory: "results",
+  tracesDirectory: "traces",
+  allowThrottling: !args.nothrottling,
+};
+
+let resultsDirectory = args.browser ? "./results_client_" + args.browser : "./results";
+
+async function main() {
+  let frameworks = await initializeFrameworks(benchmarkOptions);
+
+  let resultJS = "import {RawResult} from './Common';\n\nexport const results: RawResult[]=[";
+
+  let allBenchmarks: Array<BenchmarkInfo> = [];
+  let jsonResult: { framework: string; benchmark: string; values: { [key: string]: number[] } }[] = [];
+
+  for (const benchmarkInfo of benchmarkInfos) {
+    if (args.browser) {
+      if (benchmarkInfo.type == BenchmarkType.CPU) {
+        allBenchmarks.push(benchmarkInfo);
+      }
+    } else {
+      if (benchmarkInfo.type == BenchmarkType.STARTUP_MAIN) {
+        allBenchmarks = [...allBenchmarks, ...subbenchmarks];
+      } else {
+        allBenchmarks.push(benchmarkInfo);
+      }
+    }
+  }
+
+  for (const framework of frameworks) {
+    for (const benchmarkInfo of allBenchmarks) {
+      if (!args.browser || framework.keyed) {
+        let name = `${fileName(framework, benchmarkInfo)}`;
+        let file = `${resultsDirectory}/${name}`;
+        if (fs.existsSync(file)) {
+          let data: JsonResult = JSON.parse(
+            fs.readFileSync(file, {
+              encoding: "utf8",
+            })
+          );
+
+          let values: { [k: string]: number[] } = {};
+          for (let key of Object.keys(data.values)) {
+            let vals = data.values[key].values.filter((v) => v != null);
+            values[key] = vals;
+            if (vals.some((v) => v == null)) {
+              console.log(
+                `Found null value for ${framework.fullNameWithKeyedAndVersion} and benchmark ${benchmarkInfo.id}`
+              );
+            }
+            if (
+              benchmarkInfo.type === BenchmarkType.CPU &&
+              vals.length != config.NUM_ITERATIONS_FOR_BENCHMARK_CPU + benchmarkInfo.additionalNumberOfRuns
+            ) {
+              console.log(
+                `WARNING: for ${framework.uri} and benchmark ${benchmarkInfo.id} count was ${vals.length}. We expected ${config.NUM_ITERATIONS_FOR_BENCHMARK_CPU}`
+              );
+            } else if (
+              benchmarkInfo.type === BenchmarkType.MEM &&
+              vals.length != config.NUM_ITERATIONS_FOR_BENCHMARK_MEM
+            ) {
+              console.log(
+                `WARNING: for ${framework.uri} and benchmark ${benchmarkInfo.id} count was ${vals.length}. We expected ${config.NUM_ITERATIONS_FOR_BENCHMARK_MEM}`
+              );
+            } else if (
+              benchmarkInfo.type === BenchmarkType.STARTUP &&
+              vals.length != config.NUM_ITERATIONS_FOR_BENCHMARK_STARTUP
+            ) {
+              console.log(
+                `WARNING: for ${framework.uri} and benchmark ${benchmarkInfo.id} count was ${vals.length}. We expected ${config.NUM_ITERATIONS_FOR_BENCHMARK_STARTUP}`
+              );
+            }
+          }
+          let result: any = {
+            f: data.framework,
+            b: data.benchmark,
+            v: values,
+          };
+          let resultNice = {
+            framework: data.framework,
+            benchmark: data.benchmark,
+            values: values,
+          };
+
+          resultJS += "\n" + JSON.stringify(result) + ",";
+          jsonResult.push(resultNice);
+        } else {
+          console.log("MISSING FILE", file);
+        }
+      }
+    }
+  }
+
+  resultJS += "];\n";
+  resultJS +=
+    "export const frameworks = " +
+    JSON.stringify(
+      frameworks.map((f) => ({
+        name: f.fullNameWithKeyedAndVersion,
+        dir: (f.keyed ? "keyed/" : "non-keyed/") + f.name,
+        keyed: f.keyed,
+        issues: f.issues && f.issues.length > 0 ? f.issues : undefined,
+        frameworkHomeURL: f.frameworkHomeURL ?? "",
+      }))
+    ) +
+    ";\n";
+  let formattedBenchmarks = allBenchmarks.map((b) => ({
+    id: b.id,
+    label: b.label,
+    description: b.description(slowDownFactor(b.id, true)),
+    type: b.type,
+  }));
+  resultJS += "export const benchmarks = " + JSON.stringify(formattedBenchmarks) + ";\n";
+
+  fs.writeFileSync("../webdriver-ts-results/src/results.ts", resultJS, {
+    encoding: "utf8",
+  });
+  fs.writeFileSync("./results.json", JSON.stringify(jsonResult), {
+    encoding: "utf8",
+  });
+}
+
+try {
+  await main();
+} catch (error) {
+  console.log("Error processing results", error);
+  process.exit(1);
+}
---
+++ webdriver-ts/src/isCspCompliant.ts
@@ -0,0 +1,149 @@
+import yargs from "yargs";
+import {
+  checkElementContainsText,
+  checkElementExists,
+  clickElement,
+  startBrowser,
+} from "./playwrightAccess.js";
+import { config, FrameworkData, initializeFrameworks, BenchmarkOptions } from "./common.js";
+
+let args: any = yargs(process.argv)
+  .usage(
+    "$0 [--framework Framework1 Framework2 ...] [--benchmark Benchmark1 Benchmark2 ...] [--chromeBinary path] \n or: $0 [directory1] [directory2] .. [directory3]"
+  )
+  .help("help")
+  .boolean("headless")
+  .default("headless", false)
+  .array("framework")
+  .array("benchmark")
+  .string("chromeBinary").argv;
+
+console.log("args", args);
+
+console.log("HEADLESS***", args.headless);
+
+let benchmarkOptions: BenchmarkOptions = {
+  port: 8080,
+  host: "localhost",
+  browser: args.browser,
+  remoteDebuggingPort: 9999,
+  chromePort: 9998,
+  headless: args.headless,
+  chromeBinaryPath: args.chromeBinary,
+  numIterationsForCPUBenchmarks:
+    config.NUM_ITERATIONS_FOR_BENCHMARK_CPU +
+    config.NUM_ITERATIONS_FOR_BENCHMARK_CPU_DROP_SLOWEST_COUNT,
+  numIterationsForMemBenchmarks: config.NUM_ITERATIONS_FOR_BENCHMARK_MEM,
+  numIterationsForStartupBenchmark: config.NUM_ITERATIONS_FOR_BENCHMARK_STARTUP,
+  batchSize: 1,
+  resultsDirectory: "results",
+  tracesDirectory: "traces",
+  allowThrottling: !args.nothrottling,
+};
+
+let allArgs = args._.length <= 2 ? [] : args._.slice(2, args._.length);
+
+console.log("args.framework", args.framework, !args.framework);
+
+let matchesDirectoryArg = (directoryName: string) =>
+  allArgs.length === 0 || allArgs.some((arg: string) => arg == directoryName);
+
+async function runBench(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  frameworkNames: string[] // Not used in the function, but is used when calling the function in other files
+) {
+  let runFrameworks;
+  runFrameworks = await initializeFrameworks(benchmarkOptions, matchesDirectoryArg);
+  console.log(
+    "Frameworks that will be checked",
+    runFrameworks.map((f) => f.fullNameWithKeyedAndVersion).join(" ")
+  );
+
+  let allCorrect = true;
+
+  let browser = await startBrowser(benchmarkOptions);
+  let page = await browser.newPage();
+  try {
+    await page.goto(`http://${benchmarkOptions.host}:${benchmarkOptions.port}/csp/enable`);
+  } finally {
+    await page.close();
+    await browser.close();
+  }
+
+  console.log("*** headless", benchmarkOptions.headless);
+
+  for (let framework of runFrameworks) {
+    let browser = await startBrowser(benchmarkOptions);
+    let page = await browser.newPage();
+    try {
+      await page.goto(
+        `http://${benchmarkOptions.host}:${benchmarkOptions.port}/${framework.uri}/index.html`,
+        {
+          waitUntil: "networkidle",
+        }
+      );
+      try {
+        await checkElementExists(page, "#add");
+      } catch {
+        console.log(
+          `CSP test failed for ${framework.fullNameWithKeyedAndVersion} - during load`
+        );
+      }
+      await clickElement(page, "#add");
+      try {
+        await checkElementContainsText(
+          page,
+          "tbody>tr:nth-of-type(1000)>td:nth-of-type(1)",
+          "1000"
+        );
+      } catch {
+        console.log(
+          `CSP test failed for ${framework.fullNameWithKeyedAndVersion} - when clicking`
+        );
+      }
+    } catch {
+      //console.log("ERROR running " + runFrameworks[i].fullNameWithKeyedAndVersion, e);
+      allCorrect = false;
+    } finally {
+      try {
+        await page.close();
+        await browser.close();
+      } catch {
+        console.log("error calling driver.quit - ignoring this exception");
+      }
+    }
+  }
+
+  browser = await startBrowser(benchmarkOptions);
+  page = await browser.newPage();
+  try {
+    await page.goto(`http://${benchmarkOptions.host}:${benchmarkOptions.port}/csp`);
+    const extractedText = await page.$eval("*", (el: any) => el.innerText);
+    console.log(extractedText);
+    let failed = JSON.parse(extractedText);
+    console.log("CSP check failed for the following frameworks:\n", failed.join("\n"));
+  } finally {
+    await page.close();
+    await browser.close();
+  }
+
+  if (!allCorrect) process.exit(1);
+}
+
+let runFrameworks = (args.framework && args.framework.length > 0 ? args.framework : [""]).map(
+  (v: string) => v.toString()
+);
+
+async function main() {
+  if (args.help) {
+    // yargs.showHelp();
+  } else {
+    await runBench(runFrameworks);
+  }
+}
+
+try {
+  await main()
+} catch (error) {
+  console.log("Error in isKeyed", error);
+}
