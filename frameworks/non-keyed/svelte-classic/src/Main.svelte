91eb17b3a64ac9cdcd4ab3e0809eeac3829d08c1
Birk Skyum <birk.skyum@pm.me>
2023-11-17 20:59:43 +0100

    keep runes / classic svelte implementation

--- frameworks/non-keyed/svelte-classic/src/Main.svelte
+++ frameworks/non-keyed/svelte-classic/src/Main.svelte
@@ -1,7 +1,7 @@
 <div class="jumbotron">
 	<div class="row">
 		<div class="col-md-6">
-			<h1>Svelte (non-keyed)</h1>
+			<h1>Svelte Classic (non-keyed)</h1>
 		</div>
 		<div class="col-md-6">
       <div class="row">
