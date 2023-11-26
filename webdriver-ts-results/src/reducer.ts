7cdc2c6167b8bbec5f9d4cd172ac9fa742378bf6
Stefan Krause <stefan.krause@gmx.at>
2023-11-26 20:56:21 +0100

    improve handling of missing values

--- webdriver-ts-results/src/reducer.ts
+++ webdriver-ts-results/src/reducer.ts
@@ -39,20 +39,29 @@ const results: Result[] = rawResults.map((result) => {
     values[key] = vals;
   }
   if (result.v[CpuDurationMode.Total] && result.v[CpuDurationMode.Script] && result.v[CpuDurationMode.Render]) {
-    if (result.v[CpuDurationMode.Total].length !== result.v[CpuDurationMode.Script].length)
-      throw "Lengths must match";
-    let r = [];
-    for (let i = 0; i < result.v[CpuDurationMode.Total].length; i++) {
-      r.push(result.v[CpuDurationMode.Total][i] - result.v[CpuDurationMode.Script][i]);
+    if (result.v[CpuDurationMode.Total].length !== result.v[CpuDurationMode.Script].length) {
+      const vals = {
+        mean: NaN,
+        median: NaN,
+        standardDeviation: NaN,
+        values: [NaN],
+      };
+      debugger;
+      values[CpuDurationMode.BrowserOnly] = vals;
+    } else {
+      let r = [];
+      for (let i = 0; i < result.v[CpuDurationMode.Total].length; i++) {
+        r.push(result.v[CpuDurationMode.Total][i] - result.v[CpuDurationMode.Script][i]);
+      }
+      const vals = {
+        mean: jStat.mean(r),
+        median: jStat.median(r),
+        standardDeviation: jStat.stdev(r, true),
+        values: r,
+      };
+      values[CpuDurationMode.BrowserOnly] = vals;
     }
-    const vals = {
-      mean: jStat.mean(r),
-      median: jStat.median(r),
-      standardDeviation: jStat.stdev(r, true),
-      values: r,
-    };
     // if (result.f==='miso-v1.4.0-keyed' || result.f==='vanillajs-keyed') debugger;
-    values[CpuDurationMode.BrowserOnly] = vals;
   }
 
   return { framework: result.f, benchmark: result.b, results: values };
