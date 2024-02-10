e4f8b88383046c0a1b83557fb8b376a35574c6bd
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-02-10 22:10:21 +0800

    cleanup & upgrade deps

--- frameworks/non-keyed/vue-vapor/vite.config.js
+++ frameworks/non-keyed/vue-vapor/vite.config.js
@@ -1,19 +1,12 @@
-import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
       vapor: true,
     }),
   ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
   build: {
     rollupOptions: {
       input: "src/main.js",
