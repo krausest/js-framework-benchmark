b6da9410c19e50514344456d9d998102f7cbbf95
Daniil Zhuravlev <nakrovati@outlook.com>
2024-03-01 02:11:42 +0400

    chore: put common styles into a separate file

--- webdriver-ts-results/src/index.tsx
+++ webdriver-ts-results/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import App from "./App.tsx";
 import { createRoot } from "react-dom/client";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
