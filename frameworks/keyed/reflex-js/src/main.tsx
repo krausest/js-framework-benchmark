ff94983bc942a30f421b3580feb55a5152fd13e4
Alexis Bouhet <alexis@les-hallucines.studio>
2024-03-19 13:04:08 +0100

    Renamed + patched swap + new version

--- frameworks/keyed/reflex-js/src/main.tsx
+++ frameworks/keyed/reflex-js/src/main.tsx
@@ -120,11 +120,12 @@ function Row ( props ) {
 
 // ----------------------------------------------------------------------------- JUMBOTRON
 
+Jumbotron.isFactory = false
 function Jumbotron () {
 	return <div class="jumbotron">
 		<div class="row">
 			<div class="col-md-6">
-				<h1>Reflex-DOM keyed</h1>
+				<h1>Reflex-JS keyed</h1>
 			</div>
 			<div class="col-md-6">
 				<div class="row">
