800e16fc3cbf76155275dbb3c25d1d0a8ea9a597
Dominic Gannaway <dg@domgan.com>
2024-03-29 13:24:27 +0000

    bump version

--- frameworks/keyed/svelte/rollup.config.js
+++ frameworks/keyed/svelte/rollup.config.js
@@ -1,21 +1,24 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import terser from '@rollup/plugin-terser';
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 
-const plugins = [resolve(), svelte({
-  emitCss: false,
-})];
+const plugins = [
+  resolve({ browser: true }),
+  svelte({
+    emitCss: false,
+  }),
+];
 
 if (process.env.production) {
   plugins.push(terser());
 }
 
 export default {
-  input: 'src/main.js',
+  input: "src/main.js",
   output: {
-    file: 'dist/main.js',
-    format: 'iife',
-    name: 'main'
+    file: "dist/main.js",
+    format: "iife",
+    name: "main",
   },
-  plugins
+  plugins,
 };
