9fcbe016d312baaf4c2a14dff3d5f2fc2ffbe290
Stefan Krause <stefan.krause@gmx.at>
2024-05-07 23:24:51 +0200

    remove default delay

--- webdriver-ts/src/common.ts
+++ webdriver-ts/src/common.ts
@@ -77,7 +77,7 @@ export let config = {
   WRITE_RESULTS: true,
   ALLOW_BATCHING: true,
   BENCHMARK_RUNNER: BenchmarkRunner.PUPPETEER,
-  PUPPETEER_WAIT_MS: 1000,
+  PUPPETEER_WAIT_MS: 0,
 };
 export type Config = typeof config;
 
