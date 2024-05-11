8ff11f80f61bdcbd63bb65a1359214c8f2a0fac0
markersunny <166430764+markersunny@users.noreply.github.com>
2024-05-11 21:57:38 +0100

    Non-keyed implementation
    - added non-keyed implementation for Vanillajs-3
    - small code enhancements
    - semantic bug fix (in remove) for the keyed implementation.

--- frameworks/non-keyed/vanillajs-3/src/Main.js
+++ frameworks/non-keyed/vanillajs-3/src/Main.js
@@ -12,9 +12,30 @@ function* _random(n) {
 const data = [], nTemplates = (n) => 10, tbody = document.getElementsByTagName('tbody')[0];
 let index = 1, i, lbl, selected;
 
-function create(n = 1000) { if (data.length) clear(); append(n); }
+function create(n = 1000) { 
+    if (data.length < n) { set(data.length); append(n - data.length) }
+    else {
+        set(data.length);
+        if (data.length > n) {
+            data.length = n;
+            const rg = document.createRange();
+            rg.setStartBefore(tbody.children[n]); 
+            rg.setEndAfter(tbody.lastElementChild);
+            rg.deleteContents();
+        }
+    }
+}
+function set(n) {
+    const indices = tbody.querySelectorAll('td:first-child'); 
+    const labels = tbody.querySelectorAll('a.lbl'); 
+    const [r1, r2, r3] = _random(n);
+    for (i = 0; i < n; i++) {
+        indices[i].firstChild.nodeValue = index++;
+        labels[i].firstChild.nodeValue = data[i] = `${adjectives[r1[i]]} ${colours[r2[i]]} ${nouns[r3[i]]}`;
+    }
+}
 function append(n = 1000) {
-    const [r1, r2, r3] = _random(n), nt = nTemplates(n), arr = new Array(nt);; let j = 0;
+    const [r1, r2, r3] = _random(n), nt = nTemplates(n); let j = 0;
     const itemTemplate = document.getElementById('itemTemplate').content;
     while (nt >= itemTemplate.children.length * 2) itemTemplate.appendChild(itemTemplate.cloneNode(true));
     while (nt > itemTemplate.children.length) itemTemplate.appendChild(itemTemplate.firstElementChild.cloneNode(true));
@@ -25,9 +46,8 @@ function append(n = 1000) {
     while ((n -= nt) >= 0) {
         for (i = 0; i < nt; i++, j++) {
             ids[i].nodeValue = index++;
-            labels[i].nodeValue = arr[i] =  `${adjectives[r1[j]]} ${colours[r2[j]]} ${nouns[r3[j]]}`    
+            data.push(labels[i].nodeValue = `${adjectives[r1[j]]} ${colours[r2[j]]} ${nouns[r3[j]]}`)    
         }
-        data.push(...arr);
         tbody.appendChild(itemTemplate.cloneNode(true));
     }
 }
@@ -38,10 +58,12 @@ function update() {
 function clear() { data.length = 0; tbody.textContent = '' }
 
 function swap() {
-    if (data.length < 999) return; const first = tbody.firstElementChild;
+    if (data.length < 999) return;
     [data[1], data[998]] = [data[998], data[1]];
-    tbody.insertBefore(tbody.insertBefore(first.nextElementSibling, 
-        tbody.children[998]).nextElementSibling, first.nextElementSibling);
+    const [id1, lbl1] = tbody.children[1].querySelectorAll('td:first-child, a.lbl');
+    const [id998, lbl998] = tbody.children[998].querySelectorAll('td:first-child, a.lbl');
+    [id1.firstChild.nodeValue, id998.firstChild.nodeValue] = [id998.firstChild.nodeValue, id1.firstChild.nodeValue ];
+    [lbl1.firstChild.nodeValue, lbl998.firstChild.nodeValue] = [lbl998.firstChild.nodeValue, lbl1.firstChild.nodeValue ]    
 }
 tbody.onclick = (e) => {
     e.preventDefault; e.stopPropagation;
@@ -52,9 +74,10 @@ tbody.onclick = (e) => {
             if (selected) selected.className = "";
             element.className = "danger"; selected = element
         }
-    } else if (e.target.matches('span.remove')) { 
+    } else if (e.target.matches('span.remove')) { let temp;
         const element = e.target.parentNode.parentNode.parentNode;
-        data.splice(Array.prototype.indexOf.call(tbody.children, element), 1) && tbody.removeChild(element);
+        data.splice(Array.prototype.indexOf.call(tbody.children, element), 1);
+        tbody.removeChild(element);
     }
 }
 for (let [key, fn] of Object.entries({
