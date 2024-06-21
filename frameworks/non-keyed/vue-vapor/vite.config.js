d5e4ac21cc36f05c8a24192754d0741388275e58
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-06-22 00:51:06 +0800

    Update Vue, Vue-Vapor

--- frameworks/non-keyed/vue-vapor/vite.config.js
+++ frameworks/non-keyed/vue-vapor/vite.config.js
@@ -2,11 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [
-    vue({
-      vapor: true,
-    }),
-  ],
+  plugins: [vue()],
   build: {
     rollupOptions: {
       input: "src/main.js",
