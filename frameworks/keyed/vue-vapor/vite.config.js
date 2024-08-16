ff3b9f12319953a1c8bb78cbbc6665d740614b12
zhiyuanzmj <260480378@qq.com>
2024-08-16 08:17:30 +0800

    feat: add vue-jsx-vapor

--- frameworks/keyed/vue-vapor/vite.config.js
+++ frameworks/keyed/vue-vapor/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import Inspect from 'vite-plugin-inspect'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), Inspect()],
   build: {
     rollupOptions: {
       input: "src/main.js",
