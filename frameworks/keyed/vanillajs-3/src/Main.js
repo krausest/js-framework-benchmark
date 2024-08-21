2262279fd363b926dd695c9719dd744e00d6bd0c
markersunny <166430764+markersunny@users.noreply.github.com>
2024-08-21 03:37:16 +0100

    Updated VanillaJS-3 and Deleight keyed implementations
    I tried to make them better. Nice-looking local results.

--- frameworks/keyed/vanillajs-3/src/Main.js
+++ frameworks/keyed/vanillajs-3/src/Main.js
@@ -1,70 +1,53 @@
 "use strict";
-(function() {
-    let index = 1, data = [], labels = null, invalidLabels = true, selected = null;
-    const tbody = document.getElementsByTagName('tbody')[0];
+const adjectives = ["pretty", "large", "big", "small", "tall", "short", "long", "handsome", "plain", "quaint", "clean", "elegant", "easy", "angry", "crazy", "helpful", "mushy", "odd", "unsightly", "adorable", "important", "inexpensive", "cheap", "expensive", "fancy"];
+const colours = ["red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black", "orange"];
+const nouns = ["table", "chair", "house", "bbq", "desk", "car", "pony", "cookie", "sandwich", "burger", "pizza", "mouse", "keyboard"];
 
-    const adjectives = ["pretty", "large", "big", "small", "tall", "short", "long", "handsome", "plain", "quaint", "clean", "elegant", "easy", "angry", "crazy", "helpful", "mushy", "odd", "unsightly", "adorable", "important", "inexpensive", "cheap", "expensive", "fancy"];
-    const colours = ["red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black", "orange"];
-    const nouns = ["table", "chair", "house", "bbq", "desk", "car", "pony", "cookie", "sandwich", "burger", "pizza", "mouse", "keyboard"];
-    const l1 = adjectives.length, l2 = colours.length, l3 = nouns.length;
-    const nts = (n) => Math.round(n / 100)
+const tbody = document.querySelector('tbody'), l1 = adjectives.length, l2 = colours.length, l3 = nouns.length;
+let index = 1, op = null, c1 = null, c2 = null, c998 = null, data = [], selected = null;
 
-    function run(n = 1000) { if (data.length) clear(); add(n); }
-    function runlots() { run(10000) }
-    function add(n = 1000) {
-        const nt = nts(n);
-        let i, j = 0, r1, r2, r3;;
-
-        const itemTemplates = document.getElementById(`t${n}`).content   // .cloneNode(true);
-        if (itemTemplates.children.length < nt) {
-            const itemTemplate = itemTemplates.firstElementChild;
-            while (nt >= itemTemplates.children.length * 2) itemTemplates.appendChild(itemTemplates.cloneNode(true));
-            while (nt > itemTemplates.children.length) itemTemplates.appendChild(itemTemplate.cloneNode(true));;
+const app = {
+    run(n=1000) { if (data.length) app.clear(); app.add(n); },
+    runlots() { app.run(10000) },
+    add(n = 1000) {
+        const item = document.querySelector('template').content.firstElementChild;
+        const id = item.firstElementChild.firstChild, lbl = item.querySelector('a').firstChild;
+        for (let i = 0; i < n; i++) {
+            id.nodeValue = index++;
+            data.push(lbl.nodeValue = `${adjectives[Math.round(Math.random() * 1000) % l1]} ${colours[Math.round(Math.random() * 1000) % l2]} ${nouns[Math.round(Math.random() * 1000) % l3]}`);
+            tbody.appendChild(item.cloneNode(true));
         }
-        const ids = Array.prototype.map.call(itemTemplates.querySelectorAll(`td:first-child`), i => i.firstChild)
-        const labels = Array.prototype.map.call(itemTemplates.querySelectorAll(`a.lbl`), i => i.firstChild);
-        
-        while ((n -= nt) >= 0) {
-            for (i = 0; i < nt; i++, j++) {
-                r1 = Math.round(Math.random() * 1000) % l1;
-                r2 = Math.round(Math.random() * 1000) % l2;
-                r3 = Math.round(Math.random() * 1000) % l3;
-                ids[i].nodeValue = index++;
-                data.push(labels[i].nodeValue = `${adjectives[r1]} ${colours[r2]} ${nouns[r3]}`);
-            }
-            tbody.appendChild(itemTemplates.cloneNode(true));
-        } invalidLabels = true;
-    };
-    function update() {
-        if (invalidLabels) labels = tbody.querySelectorAll('a.lbl'); invalidLabels = false;
-        const length = labels.length;
-        let i; for (i = 0; i < length; i += 10) labels[i].firstChild.nodeValue = data[i] += ' !!!';
-    };
-    function clear() { tbody.textContent = ''; data = []; invalidLabels = true; };
-    function swaprows() {
-        if (tbody.children.length < 999) return; invalidLabels = true;
-        const first = tbody.firstElementChild;
-        [data[1], data[998]] = [data[998], data[1]];
-        tbody.insertBefore(tbody.insertBefore(first.nextElementSibling, 
-            tbody.children[998]).nextElementSibling, first.nextElementSibling);
-    };
-    tbody.onclick = (e) => {
-        e.stopPropagation;
-        if (e.target.matches('a.lbl')) {
-            e.preventDefault;
-            const element = e.target.parentNode.parentNode;
-            if (element === selected) selected.className = selected.className ? "" : "danger";
-            else {
-                if (selected) selected.className = "";
-                element.className = "danger"; selected = element;
-            }
-        } else if (e.target.matches('span.remove')) { 
-            const element = e.target.parentNode.parentNode.parentNode;
-            const index = Array.prototype.indexOf.call(tbody.children, element);
-            element.remove(); data.splice(index, 1); invalidLabels = true;
+    },
+    update() {
+        for (let i = 0, item; i < data.length; i += 10) {
+            if (op === 'update' && item?.hasOwnProperty('next')) item = item.next;
+            else if (item) { item.next = tbody.childNodes[i]; item = item.next }
+            else item = tbody.childNodes[i];
+            if (!item.hasOwnProperty('el')) item.el = item.querySelector('a').firstChild;
+            item.el.nodeValue = data[i] += ' !!!';
         }
+    },
+    clear() { tbody.textContent = ''; data = [] },
+    swaprows() {
+        if (data.length < 999) return;
+        const d1 = data[1]; data[1] = data[998]; data[998] = d1;
+        if (op === 'swaprows') { const temp = c1; c1 = c998; c998 = temp }
+        else { c1 = tbody.children[1]; c2 = c1.nextElementSibling; c998 = tbody.children[998] }
+        tbody.insertBefore(c1, c998); tbody.insertBefore(c998, c2);
     }
-    for (let [key, value] of Object.entries({run, runlots, add, update, clear, swaprows})) {
-        document.getElementById(key).onclick = (e) => { e.stopPropagation(); value(); }
+}
+
+tbody.onclick = (e) => {
+    e.stopPropagation; e.preventDefault; op = 'null';
+    if (e.target.tagName === 'A') {
+        const element = e.target.parentNode.parentNode;
+        if (selected) selected.className = '';
+        selected = (element === selected)? null: element;
+        if (selected) selected.className = 'danger';
+    } else if (e.target.tagName === 'SPAN') { 
+        const element = e.target.parentNode.parentNode.parentNode;
+        const index = Array.prototype.indexOf.call(tbody.children, element);
+        element.remove(); data.splice(index, 1);
     }
-})();
+}
+document.querySelector('#app-actions').onclick = (e) => { e.stopPropagation(); e.preventDefault(); app[e.target.id](); op = e.target.id };
