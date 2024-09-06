6e1c05b587eabc11da82eee1d8f7510b8da87919
Stefan Krause <stefan.krause@gmx.at>
2024-09-06 20:13:51 +0000

    fix build errors

--- frameworks/keyed/vuerx-jsx/rollup.config.js
+++ frameworks/keyed/vuerx-jsx/rollup.config.js
@@ -1,7 +1,7 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
 import { babel } from "@rollup/plugin-babel";
-import terser from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 const isProduction = process.env.BUILD === "production";
 
