e38ee971ae361ef6c0509c12d21c751667607e96
Lucas Segurado <lsegurado1996@gmail.com>
2024-11-21 10:08:30 +0100

    Removed entrypoint

--- frameworks/keyed/michijs/michi.config.ts
+++ frameworks/keyed/michijs/michi.config.ts
@@ -2,7 +2,6 @@ import type { ServerConfigFactory } from '@michijs/dev-server';
 
 export const config: ServerConfigFactory = () => ({
   esbuildOptions: {
-    entryPoints: ['src/index.tsx'],
     outdir: 'dist',
     legalComments: 'none'
   }
