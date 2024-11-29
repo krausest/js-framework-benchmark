5144804b7e6f3271dfce64f03fd5276169fd005b
Lucas Segurado <lsegurado1996@gmail.com>
2024-11-29 10:17:55 +0100

    Small bundle change

--- frameworks/keyed/michijs/michi.config.ts
+++ frameworks/keyed/michijs/michi.config.ts
@@ -3,7 +3,9 @@ import type { ServerConfigFactory } from '@michijs/dev-server';
 export const config: ServerConfigFactory = () => ({
   esbuildOptions: {
     outdir: 'dist',
-    legalComments: 'none'
+    legalComments: 'none',
+    define: undefined,
+    splitting: false
   }
 });
 
