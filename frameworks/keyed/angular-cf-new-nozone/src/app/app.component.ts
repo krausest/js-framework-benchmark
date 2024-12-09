c4f7f2e1f015e57fbb76f8b839fafa8e5feacc58
Stefan Krause <stefan.krause@gmx.at>
2024-12-09 20:40:20 +0000

    add angular-cf-zoneless

---
+++ frameworks/keyed/angular-cf-new-nozone/src/app/app.component.ts
@@ -0,0 +1,87 @@
+import { NgFor } from '@angular/common';
+import { ChangeDetectionStrategy, Component, VERSION } from '@angular/core';
+
+interface Data {
+    id: number;
+    label: string;
+}
+
+const adjectives = ["pretty", "large", "big", "small", "tall", "short", "long", "handsome", "plain", "quaint", "clean", "elegant", "easy", "angry", "crazy", "helpful", "mushy", "odd", "unsightly", "adorable", "important", "inexpensive", "cheap", "expensive", "fancy"];
+const colours = ["red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black", "orange"];
+const nouns = ["table", "chair", "house", "bbq", "desk", "car", "pony", "cookie", "sandwich", "burger", "pizza", "mouse", "keyboard"];
+        
+
+@Component({
+    selector: 'app-root',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [NgFor],
+    templateUrl: './app.component.html'
+})
+export class AppComponent {
+    data: Array<Data> = [];
+    selected?: number = undefined;
+    id: number = 1;
+    backup?: Array<Data> = undefined;
+    version = VERSION.full;
+
+    buildData(count: number = 1000): Array<Data> {
+        var data: Array<Data> = new Array(count);
+        for (var i = 0; i < count; i++) {
+            data[i]={ id: this.id, label: `${adjectives[this._random(adjectives.length)]} ${colours[this._random(colours.length)]} ${nouns[this._random(nouns.length)]}` };
+            this.id++;
+        }
+        return data;
+    }
+
+    _random(max: number) {
+        return Math.round(Math.random() * 1000) % max;
+    }
+
+    itemById(index: number, item: Data) {
+        return item.id;
+    }
+
+    select(item: Data, event: Event) {
+        event.preventDefault();
+        this.selected = item.id;
+    }
+
+    delete(item: Data, event: Event) {
+        event.preventDefault();
+        for (let i = 0, l = this.data.length; i < l; i++) {
+            if (this.data[i].id === item.id) {
+                this.data.splice(i, 1);
+                break;
+            }
+        }
+    }
+
+    run() {
+        this.data = this.buildData();
+    }
+
+    add() {
+        this.data = this.data.concat(this.buildData(1000));
+    }
+
+    update() {
+        for (let i = 0; i < this.data.length; i += 10) {
+            this.data[i].label += ' !!!';
+        }
+    }
+    runLots() {
+        this.data = this.buildData(10000);
+        this.selected = undefined;
+    }
+    clear() {
+        this.data = [];
+        this.selected = undefined;
+    }
+    swapRows() {
+        if (this.data.length > 998) {
+            var a = this.data[1];
+            this.data[1] = this.data[998];
+            this.data[998] = a;
+        }
+    }
+}
---
+++ frameworks/keyed/angular-cf-new-nozone/src/main.ts
@@ -0,0 +1,9 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideExperimentalZonelessChangeDetection()
+  ]
+});
