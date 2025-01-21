3cc1b23178e371d2fce94d9d0d45415d239859b9
Dwayne Charrington <dwaynecharrington@gmail.com>
2025-01-21 12:58:11 +1000

    refact(aurelia): update legacy aurelia 1 app
    The existing Aurelia 1 application was heavily outdated. This updates the app to use the latest packages and tooling processes for building the app.

--- frameworks/non-keyed/aurelia/aurelia_project/generators/component.js
+++ frameworks/non-keyed/aurelia/aurelia_project/generators/component.js
@@ -1,51 +1,51 @@
 import { inject } from 'aurelia-dependency-injection';
 import { Project, ProjectItem, CLIOptions, UI } from 'aurelia-cli';
-
-var path = require('path');
+import path from 'path';
 
 @inject(Project, CLIOptions, UI)
 export default class ElementGenerator {
-  constructor(project, options, ui) { 
+  constructor(project, options, ui) {
     this.project = project;
     this.options = options;
     this.ui = ui;
   }
 
-  execute() {
-    let self = this;
+  async execute() {
+    const name = await this.ui.ensureAnswer(
+      this.options.args[0],
+      'What would you like to call the component?'
+    );
 
-    return this.ui
-      .ensureAnswer(this.options.args[0], 'What would you like to call the component?')
-      .then(name => {
+    const subFolders = await this.ui.ensureAnswer(
+      this.options.args[1],
+      'What sub-folder would you like to add it to?\nIf it doesn\'t exist it will be created for you.\n\nDefault folder is "." relative to the source folder src/', "."
+    );
 
-        return self.ui.ensureAnswer(this.options.args[1], 'What sub-folder would you like to add it to?\nIf it doesn\'t exist it will be created for you.\n\nDefault folder is the source folder (src).', ".")
-          .then(subFolders => {
+    let fileName = this.project.makeFileName(name);
+    let className = this.project.makeClassName(name);
 
-            let fileName = this.project.makeFileName(name);
-            let className = this.project.makeClassName(name);
+    this.project.root.add(
+      ProjectItem.text(path.join(subFolders, fileName + '.js'), this.generateJSSource(className)),
+      ProjectItem.text(path.join(subFolders, fileName + '.html'), this.generateHTMLSource(className))
+    );
 
-            self.project.root.add(
-              ProjectItem.text(path.join(subFolders, fileName + ".js"), this.generateJSSource(className)),
-              ProjectItem.text(path.join(subFolders, fileName + ".html"), this.generateHTMLSource(className))
-            );
-
-            return this.project.commitChanges()
-              .then(() => this.ui.log(`Created ${name} in the '${path.join(self.project.root.name, subFolders)}' folder`));
-          });
-      });
+    await this.project.commitChanges();
+    await this.ui.log(`Created ${name} in the '${path.join(this.project.root.name, subFolders)}' folder`);
   }
 
   generateJSSource(className) {
-    return `export class ${className} {     
+    return `export class ${className} {
   constructor() {
     this.message = 'Hello world';
   }
-}`
+}
+`
   }
 
   generateHTMLSource(className) {
     return `<template>
   <h1>\${message}</h1>
-</template>`
+</template>
+`
   }
 }
