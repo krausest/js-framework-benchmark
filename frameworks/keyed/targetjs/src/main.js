4bdc46d566a034703697fb1f49c86ba8f0575d82
wasfi2 <wasfi2@hotmail.com>
2025-01-23 00:23:25 -0800

    provided a new implementation which is more DOM based

--- frameworks/keyed/targetjs/src/main.js
+++ frameworks/keyed/targetjs/src/main.js
@@ -1,60 +1,59 @@
-import { App, TModel, $Dom } from "targetj";
+import { App, TModel, $Dom, Template } from "targetj";
 import { buildData } from './data';
 
 App(new TModel("benchmark", {
     canHaveDom: false,
     runButton() {        
         return new TModel('run', {            
-            onClickEvent() {                
+            onClick() {                
                 const rows = this.getParent().findChild('rows');
                 rows.removeAll();
-                rows.activateTarget('createRows', 1000);
+                rows.activateTarget('buildData', 1000);                
+                rows.activateTarget('createRows');
             }
         });
     },    
     runAlotButton() {
         return new TModel('runlots', {
-            onClickEvent() {                
+            onClick() {                
                 const rows = this.getParent().findChild('rows');
                 rows.removeAll();
-                rows.activateTarget('createRows', 10000);
+                rows.activateTarget('buildData', 10000);                
+                rows.activateTarget('createRows');
             }
         });
     },
     addButton() {
         return new TModel('add', {
-            onClickEvent() {                
+            onClick() {                
                 const rows = this.getParent().findChild('rows');
-                rows.activateTarget('createRows', 1000);            
+                rows.activateTarget('buildData', 1000);                                
+                rows.activateTarget('createRows');            
             }
         });
     },
     updateButton() {
         return new TModel('update', {
-            onClickEvent() {
+            onClick() {
                 const rows = this.getParent().findChild('rows');
-                const length = rows.getChildren().length;
-                for (let i = 0; i < length; i += 10) {
-                    rows.getChild(i).activateTarget('updateContent');
-                };
+                rows.activateTarget('updateEvery10thRow');
             }
         });
     },
     clearButton() {
         return new TModel('clear', {
-            onClickEvent() {                
+            onClick() {                
                  this.getParent().findChild('rows').removeAll();            
             }
         });
     },
     swapRows() {
         return new TModel('swaprows', {
-            onClickEvent() {
+            onClick() {
                 const rows = this.getParent().findChild('rows');
-                const rowChildren = rows.allChildrenList;
-                if (rowChildren.length > 998) {
-                    rows.moveChild(rowChildren[1], 998);
-                    rows.moveChild(rowChildren[998], 1);
+                const rowCount = rows.$dom.elementCount();
+                if (rowCount > 998) {
+                    rows.activateTarget('swap', [1, 998]);
                 }
             }
         });        
@@ -62,49 +61,60 @@ App(new TModel("benchmark", {
     rows() {
         const rows = new TModel('rows', {
             isVisible: true, 
-            containerOverflowMode: 'always',
-            rectTop() { return this.$dom.getBoundingClientRect().top; },            
-            absY() { return this.val('rectTop') - $Dom.getWindowScrollTop(); },            
             domHolder: true,
-            onDomEvent: [ 'rectTop', 'absY' ], 
-            onWindowScrollEvent: 'absY',
-            _createRows: {
-                parallel: true,
-                cycles: 4,
-                value() {
-                    buildData(this._createRows / 5).forEach(data => {
-                        this.addChild(new TModel('row', {
-                            keepEventDefault: true,
-                            baseElement: 'tr',
-                            defaultStyling: false,
-                            excludeHeight: true,
-                            height: 36,
-                            canDeleteDom: false,
-                            textOnly: false,
-                            domHolder: true,
-                            _css() { return this._css; },
-                            onClickEvent(target) {
-                                if (target?.className?.endsWith('remove')) {
-                                    rows.removeChild(this); 
-                                } else {
-                                    rows.val('selectedRow')?.activateTarget('css', '');
-                                    rows.val('selectedRow', this);
-                                    this.activateTarget('css', 'danger');
-                                }
-                            },
-                            _updateContent() {
-                                this.$dom.child(1).children[0].textContent += ' !!!';
-                            },
-                            html: `<td class="col-md-1">${data.id}</td>
-                                   <td class="col-md-4"><a>${data.label}</a></td>
-                                   <td class="col-md-1"><a><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a></td>
-                                   <td class="col-md-6"></td>`
-                        }));
-                    }); 
+            rowTemplate() {
+                const template = document.createElement('template');
+                template.innerHTML = `
+                    <tr>
+                        <td class="col-md-1 id-cell"></td>
+                        <td class="col-md-4 label-cell"><a></a></td>
+                        <td class="col-md-1 remove-cell"><a><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a></td>
+                        <td class="col-md-6"></td>
+                    </tr>
+                `;
+                return template;
+            },
+            _buildData() {
+                return buildData(this._buildData);                
+            },
+            _createRows() {
+                const fragment = document.createDocumentFragment();
+                this.val('buildData').forEach((data, index) => {
+                    const rowClone = this.val('rowTemplate').content.cloneNode(true);
+                    rowClone.querySelector('tr').setAttribute('data-id', `${index}`);
+                    rowClone.querySelector('.id-cell').textContent = data.id;
+                    rowClone.querySelector('.label-cell a').textContent = data.label;
+
+                    fragment.appendChild(rowClone);
+                });
+                this.$dom.appendElement(fragment);
+            },
+            _updateEvery10thRow() {
+                const rows = this.$dom.querySelectorAll('tr');
+
+                for (let i = 0; i < rows.length; i += 10) {
+                    const labelCell = rows[i].querySelector('.label-cell a');
+                    if (labelCell) {
+                        labelCell.textContent += ' !!!';
+                    }
+                }                
+            },
+            _swap() {
+                const row1 = this.$dom.querySelector(`[data-id="${this._swap[0]}"]`);
+                const row2 = this.$dom.querySelector(`[data-id="${this._swap[1]}"]`);
+                this.$dom.swapElements(row1, row2);
+            },
+            onClick(target) {
+                const rowElement = target.closest('tr');
+                if (target?.className?.endsWith('remove')) {
+                    this.$dom.removeElement(rowElement); 
+                } else {
+                    this.val('selectedRow', rowElement);
+                    this.val('selectedRow').setAttribute('class', 'danger');
+                    this.lastVal('selectedRow')?.setAttribute('class', '');
                 }
-            }            
+            }
         });  
-        
         return rows;
     },
     children() {
