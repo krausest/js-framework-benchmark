5aebca49d1f225a471e960e10c038b7b4a14c107
nihil-pro <Romanolga13>
2025-02-26 20:23:34 +0300

    added preact+kr-observable and update react+kr-observable

---
+++ frameworks/keyed/preact-kr-observable/rollup.config.mjs
@@ -0,0 +1,43 @@
+import { defineConfig } from 'rollup';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import babel from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
+import alias from '@rollup/plugin-alias';
+
+const plugins = [
+  commonjs(),
+  replace({
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  }),
+  babel({
+    exclude: 'node_modules/**',
+    babelHelpers: 'bundled',
+    presets: [
+      // '@babel/preset-env',
+      ['@babel/preset-react', { runtime: 'automatic' }],
+    ],
+  }),
+  alias({
+    entries:[
+      { find: 'react', replacement: 'preact/compat' },
+      { find: 'react-dom', replacement: 'preact/compat' },
+      { find: 'react/jsx-runtime', replacement: 'preact/compat' }
+    ]
+  }),
+  nodeResolve({ extensions: ['.js', '.jsx'] }),
+];
+
+if (process.env.production) {
+  plugins.push(terser());
+}
+
+export default defineConfig({
+  input: 'src/Main.jsx',
+  output: {
+    file: 'dist/main.js',
+    format: 'commonjs',
+  },
+  plugins,
+});
