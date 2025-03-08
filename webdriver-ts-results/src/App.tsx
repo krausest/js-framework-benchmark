bfc8fe37d886483a8112830be487d45f9ea70531
Stefan Krause <stefan.krause@gmx.at>
2025-03-08 21:10:36 +0100

    chrome 134 results

--- webdriver-ts-results/src/App.tsx
+++ webdriver-ts-results/src/App.tsx
@@ -30,7 +30,7 @@ const KnownIssuesList = () => {
 
 const App = () => {
   // eslint-disable-next-line no-constant-condition
-  const version = "Chrome 133.0.6943.142"
+  const version = "Chrome 134.0.6998.45"
   const disclaimer = false ? (
     <div>
       <h2>js-framework-benchmark results for {version}</h2>
