169332fb8d61bd9b06a0f16cea5275a0c47ee10c
nihil-pro <Romanolga13>
2025-03-18 20:23:18 +0300

    Performance improvement

--- frameworks/keyed/react-kr-observable/src/Main.jsx
+++ frameworks/keyed/react-kr-observable/src/Main.jsx
@@ -4,6 +4,7 @@ import { Row } from './Row';
 import { rowsStore } from './RowsStore';
 import { Fragment } from "react";
 
+
 const Button = ({ children, id, onClick }) => {
   return (
     <div className="col-sm-6 smallpad">
