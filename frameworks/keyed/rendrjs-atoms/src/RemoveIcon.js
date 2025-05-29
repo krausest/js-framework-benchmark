56c121d238822b2199c74756b9867d0822f763ac
Travis Harmon <harmtrav@gmail.com>
2025-05-29 15:19:28 -0500

    Remove rendrjs and rendrjs-atoms

--- frameworks/keyed/rendrjs-atoms/src/RemoveIcon.js
+++
@@ -1,6 +0,0 @@
-import { element } from '@rendrjs/core';
-
-export let makeIcon = preload => element('span', {
-    class: (preload ? 'preloadicon ' : '') + 'glyphicon glyphicon-remove',
-    'aria-hidden': true,
-});
