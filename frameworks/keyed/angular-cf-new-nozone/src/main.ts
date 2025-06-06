2e4eca0e3b4bf97311db3b062ae1876d0b83637e
Stefan Krause <stefan.krause@gmx.at>
2025-06-06 05:35:47 +0000

    update angular

--- frameworks/keyed/angular-cf-new-nozone/src/main.ts
+++ frameworks/keyed/angular-cf-new-nozone/src/main.ts
@@ -1,9 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideExperimentalZonelessChangeDetection()
+    provideZonelessChangeDetection()
   ]
 });
