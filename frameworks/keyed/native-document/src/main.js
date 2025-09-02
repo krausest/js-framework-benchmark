969811602f8c59158d73da36bcfb3732e8965db3
Afrocodeur <d.mamadou@miridoo.net>
2025-09-02 18:48:52 +0000

    upgrade native document to 1.0.32

--- frameworks/keyed/native-document/src/main.js
+++ frameworks/keyed/native-document/src/main.js
@@ -1,32 +1,35 @@
 import { Tr, Td, ForEachArray, Link, Span, Div, Table, TBody } from "native-document/elements";
-import { Observable } from "native-document";
 
 import Jumbotron from "./components/Jumbotron";
 import RemoveIcon from "./components/RemoveIcon";
 
 import AppService from "./service";
 
-const App = () => {
 
-  return Div({ class: 'container' }, [
-    Jumbotron(),
-    Table({ class: 'table table-hover table-striped test-data' }, [
-      TBody(
-        ForEachArray(AppService.data, (item) => {
-            return Tr( { class: { 'danger': AppService.selected.when(item.id) } }, [
-              Td({ class: 'col-md-1' }, item.id),
-              Td({ class: 'col-md-4' },
-                Link(item.label)
-              ).nd.onClick(() => AppService.select(item.id)),
-              Td({ class: 'col-md-1' }, RemoveIcon(item.id)),
-              Td({ class: 'col-md-6'})
-            ]);
-          },
-          'id')
-      ),
-      Span({ class: 'preloadicon glyphicon glyphicon-remove', 'aria-hidden': true })
-    ])
+const Attributes = {
+  table: { class: 'table table-hover table-striped test-data' },
+  cell_md_1: { class: 'col-md-1' },
+  cell_md_4: { class: 'col-md-4' },
+};
+
+const emptyCell = Td.cached({ class: 'col-md-6'});
+const TableRowBuilder = (item) => {
+  return Tr( { class: { 'danger': AppService.selected.when(item.id) } }, [
+    Td(Attributes.cell_md_1, item.id),
+    Td(Attributes.cell_md_4, Link(item.label)).nd.onClick(() => AppService.select(item.id)),
+    Td(Attributes.cell_md_1,
+      RemoveIcon(item.id).nd.onClick(() => AppService.remove(item.id))
+    ),
+    emptyCell
   ]);
-}
+};
 
-document.querySelector('#main').append(App());
+const App = Div({ class: 'container' }, [
+  Jumbotron(),
+  Table(Attributes.table,
+    TBody(ForEachArray(AppService.data, TableRowBuilder, 'id', { isParentUniqueChild: true}))
+  ),
+  Span({ class: 'preloadicon glyphicon glyphicon-remove', 'aria-hidden': true })
+]);
+
+document.getElementById('root').appendChild(App);
