f414fb2205f02d8cdbc7926ebc52f2f56fc136ba
Nigro Simone <nigro.simone@gmail.com>
2025-12-14 15:01:08 +0100

    fix: no zone

--- frameworks/keyed/angular-cf/src/main.ts
+++ frameworks/keyed/angular-cf/src/main.ts
@@ -1,5 +1,4 @@
-import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {providers: [provideZoneChangeDetection()]});
+bootstrapApplication(AppComponent);
