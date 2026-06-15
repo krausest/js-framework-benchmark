45cff81688c9b12dac1d20362fe4d6b04147f967
Mikeysax <michaelfarrell@protonmail.com>
2026-06-15 17:08:11 -0400

    fix(non-keyed/literaljs): make buttons work and pass benchmark validation

--- frameworks/non-keyed/literaljs/index.js
+++ frameworks/non-keyed/literaljs/index.js
@@ -1,165 +1,150 @@
-/** @jsx h */
+import { h, component, App } from 'literaljs';
 
-import { App, component, h } from 'literaljs';
+let idCounter = 1;
+function _random(max) { return Math.round(Math.random() * 1000) % max; }
 
-import { Store } from './store';
+const adjectives = ['pretty', 'large', 'big', 'small', 'tall', 'short', 'long', 'handsome', 'plain', 'quaint', 'clean', 'elegant', 'easy', 'angry', 'crazy', 'helpful', 'mushy', 'odd', 'unsightly', 'adorable', 'important', 'inexpensive', 'cheap', 'expensive', 'fancy'];
+const colours  = ['red', 'yellow', 'blue', 'green', 'pink', 'brown', 'purple', 'brown', 'white', 'black', 'orange'];
+const nouns    = ['table', 'chair', 'house', 'bbq', 'desk', 'car', 'pony', 'cookie', 'sandwich', 'burger', 'pizza', 'mouse', 'keyboard'];
+
+function buildData(count) {
+	const data = [];
+	for (let i = 0; i < count; i++) {
+		data.push({
+			id: idCounter++,
+			label: adjectives[_random(adjectives.length)] + ' ' + colours[_random(colours.length)] + ' ' + nouns[_random(nouns.length)]
+		});
+	}
+	return data;
+}
 
 const AppComponent = component({
-	state: { store: new Store() },
+	state: { data: [], selected: null },
 	methods() {
-		const { store } = this.getState();
 		return {
-			run() {
-				store.run();
-				this.setState({ store });
-			},
+			run() { this.setState({ data: buildData(1000), selected: null }); },
+			runLots() { this.setState({ data: buildData(10000), selected: null }); },
 			add() {
-				store.add();
-				this.setState({ store });
+				const data = this.getState().data;
+				this.setState({ data: data.concat(buildData(1000)) });
 			},
 			update() {
-				store.update();
-				this.setState({ store });
+				const data = this.getState().data.slice();
+				for (let i = 0; i < data.length; i += 10) {
+					data[i] = { ...data[i], label: data[i].label + ' !!!' };
+				}
+				this.setState({ data });
 			},
-			select(id) {
-				store.select(id);
-				this.setState({ store });
-			},
+			select(id) { this.setState({ selected: id }); },
 			delete(id) {
-				store.delete(id);
-				this.setState({ store });
+				const data = this.getState().data.filter(d => d.id !== id);
+				this.setState({ data });
 			},
-			runLots() {
-				store.runLots();
-				this.setState({ store });
-			},
-			clear() {
-				store.clear();
-				this.setState({ store });
-			},
+			clear() { this.setState({ data: [], selected: null }); },
 			swapRows() {
-				store.swapRows();
-				this.setState({ store });
+				const data = this.getState().data.slice();
+				if (data.length > 998) {
+					const tmp = data[1];
+					data[1] = data[998];
+					data[998] = tmp;
+					this.setState({ data });
+				}
 			}
 		};
 	},
 	render() {
-		const { store } = this.getState();
-		return (
-			<div class="container">
-				<div class="jumbotron">
-					<div class="row">
-						<div class="col-md-6">
-							<h1>LiteralJS (non-keyed)</h1>
-						</div>
-						<div class="col-md-6">
-							<div class="row">
-								<div class="col-sm-6 smallpad">
-									<button
-										type="button"
-										class="btn btn-primary btn-block"
-										id="run"
-										events={{ click: this.run }}
-									>
-										Create 1,000 rows
-									</button>
-								</div>
-								<div class="col-sm-6 smallpad">
-									<button
-										type="button"
-										class="btn btn-primary btn-block"
-										id="runlots"
-										events={{ click: this.runLots }}
-									>
-										Create 10,000 rows
-									</button>
-								</div>
-								<div class="col-sm-6 smallpad">
-									<button
-										type="button"
-										class="btn btn-primary btn-block"
-										id="add"
-										events={{ click: this.add }}
-									>
-										Append 1,000 rows
-									</button>
-								</div>
-								<div class="col-sm-6 smallpad">
-									<button
-										type="button"
-										class="btn btn-primary btn-block"
-										id="update"
-										events={{ click: this.update }}
-									>
-										Update every 10th row
-									</button>
-								</div>
-								<div class="col-sm-6 smallpad">
-									<button
-										type="button"
-										class="btn btn-primary btn-block"
-										id="clear"
-										events={{ click: this.clear }}
-									>
-										Clear
-									</button>
-								</div>
-								<div class="col-sm-6 smallpad">
-									<button
-										type="button"
-										class="btn btn-primary btn-block"
-										id="swaprows"
-										events={{ click: this.swapRows }}
-									>
-										Swap Rows
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<table class="table table-hover table-striped test-data">
-					<tbody id="tbody">
-						{store.data.map(item => (
-							<tr
-								class={
-									item.id === store.selected ? 'danger' : ''
-								}
-							>
-								<td class="col-md-1">{item.id}</td>
-								<td class="col-md-4">
-									<a
-										events={{
-											click: () => this.select(item.id)
-										}}
-									>
-										{item.label}
-									</a>
-								</td>
-								<td class="col-md-1">
-									<a>
-										<span
-											class="glyphicon glyphicon-remove"
-											aria-hidden="true"
-											events={{
-												click: () =>
-													this.delete(item.id)
-											}}
-										/>
-									</a>
-								</td>
-								<td class="col-md-6" />
-							</tr>
-						))}
-					</tbody>
-				</table>
-				<span
-					class="preloadicon glyphicon glyphicon-remove"
-					aria-hidden="true"
-				/>
-			</div>
-		);
+		const { data, selected } = this.getState();
+		return h('div', { class: 'container' }, [
+			h('div', { class: 'jumbotron' },
+				h('div', { class: 'row' }, [
+					h('div', { class: 'col-md-6' }, h('h1', {}, 'LiteralJS (non-keyed)')),
+					h('div', { class: 'col-md-6' },
+						h('div', { class: 'row' }, [
+							h('div', { class: 'col-sm-6 smallpad' },
+								h('button', {
+									type: 'button',
+									class: 'btn btn-primary btn-block',
+									id: 'run',
+									events: { click: () => this.run() }
+								}, 'Create 1,000 rows')
+							),
+							h('div', { class: 'col-sm-6 smallpad' },
+								h('button', {
+									type: 'button',
+									class: 'btn btn-primary btn-block',
+									id: 'runlots',
+									events: { click: () => this.runLots() }
+								}, 'Create 10,000 rows')
+							),
+							h('div', { class: 'col-sm-6 smallpad' },
+								h('button', {
+									type: 'button',
+									class: 'btn btn-primary btn-block',
+									id: 'add',
+									events: { click: () => this.add() }
+								}, 'Append 1,000 rows')
+							),
+							h('div', { class: 'col-sm-6 smallpad' },
+								h('button', {
+									type: 'button',
+									class: 'btn btn-primary btn-block',
+									id: 'update',
+									events: { click: () => this.update() }
+								}, 'Update every 10th row')
+							),
+							h('div', { class: 'col-sm-6 smallpad' },
+								h('button', {
+									type: 'button',
+									class: 'btn btn-primary btn-block',
+									id: 'clear',
+									events: { click: () => this.clear() }
+								}, 'Clear')
+							),
+							h('div', { class: 'col-sm-6 smallpad' },
+								h('button', {
+									type: 'button',
+									class: 'btn btn-primary btn-block',
+									id: 'swaprows',
+									events: { click: () => this.swapRows() }
+								}, 'Swap Rows')
+							)
+						])
+					)
+				])
+			),
+			h('table', { class: 'table table-hover table-striped test-data' },
+				h('tbody', { id: 'tbody' },
+					data.map(item =>
+						h('tr', {
+							class: item.id === selected ? 'danger' : ''
+						}, [
+							h('td', { class: 'col-md-1' }, String(item.id)),
+							h('td', { class: 'col-md-4' },
+								h('a', {
+									events: { click: () => this.select(item.id) }
+								}, item.label)
+							),
+							h('td', { class: 'col-md-1' },
+								h('a', null,
+									h('span', {
+										class: 'glyphicon glyphicon-remove',
+										'aria-hidden': 'true',
+										events: { click: () => this.delete(item.id) }
+									})
+								)
+							),
+							h('td', { class: 'col-md-6' })
+						])
+					)
+				)
+			),
+			h('span', {
+				class: 'preloadicon glyphicon glyphicon-remove',
+				'aria-hidden': 'true'
+			})
+		]);
 	}
 });
 
-new App(AppComponent, {}).mount('root');
+const app = new App(AppComponent, {});
+app.mount('root');
