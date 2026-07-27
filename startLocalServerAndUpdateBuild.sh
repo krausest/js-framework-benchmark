7ba30b7855a63b6f80ae1f3f012ed52070ce507f
Stefan Krause <stefan.krause@gmx.at>
2026-07-27 21:08:06 +0200

    improve copying files from VPS

--- startLocalServerAndUpdateBuild.sh
+++ startLocalServerAndUpdateBuild.sh
@@ -1,8 +1,8 @@
 # private file to copy the zip file from my VPS
 set -e
 git pull
-echo "running zip on js-framework-benchmark"
-ssh $VPS_HOST "cd js-framework-benchmark  && npm run zip"
+echo "running zip on $VPS_HOST"
+ssh $VPS_HOST "bash -i -c 'cd js-framework-benchmark && npm run zip'"
 scp $VPS_HOST:/home/stefan/js-framework-benchmark/build.zip .
 unzip -o build.zip
 ssh $VPS_HOST "ls -l js-framework-benchmark/build.zip"
